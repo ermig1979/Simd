f7dff1319506076b160f75c9fbdbc455be8d252d
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2013-04-03 10:11:27 +0000

    *changed macros for initialization of vector constants (for GCC);

--- trunk/src/Simd/SimdInit.h
+++ trunk/src/Simd/SimdInit.h
@@ -3,20 +3,20 @@
 *
 * Copyright (c) 2011-2013 Yermalayeu Ihar.
 *
-* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-* copies of the Software, and to permit persons to whom the Software is 
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
 * furnished to do so, subject to the following conditions:
 *
-* The above copyright notice and this permission notice shall be included in 
+* The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
@@ -28,7 +28,8 @@
 
 namespace Simd
 {
-#ifdef _MSC_VER    
+#if defined(_MSC_VER)
+
 	template <class T> SIMD_INLINE char GetChar(T value, size_t index)
     {
         return ((char*)&value)[index];
@@ -42,16 +43,52 @@ namespace Simd
 #define SIMD_AS_4CHARS(a) \
 	Simd::GetChar(int32_t(a), 0), Simd::GetChar(int32_t(a), 1), \
 	Simd::GetChar(int32_t(a), 2), Simd::GetChar(int32_t(a), 3)
-#endif//_MSC_VER
+
+#elif defined(__GNUC__)
+
+#define SIMD_CHAR_AS_LONGLONG(a) (((long long)a) & 0xFF)
+
+#define SIMD_SHORT_AS_LONGLONG(a) (((long long)a) & 0xFFFF)
+
+#define SIMD_INT_AS_LONGLONG(a) (((long long)a) & 0xFFFFFFFF)
+
+#define SIMD_LL_SET1_EPI8(a) \
+    SIMD_CHAR_AS_LONGLONG(a) | (SIMD_CHAR_AS_LONGLONG(a) >> 8) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 16) | (SIMD_CHAR_AS_LONGLONG(a) >> 24) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 32) | (SIMD_CHAR_AS_LONGLONG(a) >> 40) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 48) | (SIMD_CHAR_AS_LONGLONG(a) >> 56)
+
+#define SIMD_LL_SET2_EPI8(a, b) \
+    SIMD_CHAR_AS_LONGLONG(a) | (SIMD_CHAR_AS_LONGLONG(b) >> 8) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 16) | (SIMD_CHAR_AS_LONGLONG(b) >> 24) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 32) | (SIMD_CHAR_AS_LONGLONG(b) >> 40) | \
+    (SIMD_CHAR_AS_LONGLONG(a) >> 48) | (SIMD_CHAR_AS_LONGLONG(b) >> 56)
+
+#define SIMD_LL_SET1_EPI16(a) \
+    SIMD_SHORT_AS_LONGLONG(a) | (SIMD_SHORT_AS_LONGLONG(a) >> 16) | \
+    (SIMD_SHORT_AS_LONGLONG(a) >> 32) | (SIMD_SHORT_AS_LONGLONG(a) >> 48)
+
+#define SIMD_LL_SET2_EPI16(a, b) \
+    SIMD_SHORT_AS_LONGLONG(a) | (SIMD_SHORT_AS_LONGLONG(b) >> 16) | \
+    (SIMD_SHORT_AS_LONGLONG(a) >> 32) | (SIMD_SHORT_AS_LONGLONG(b) >> 48)
+
+#define SIMD_LL_SET1_EPI32(a) \
+    SIMD_INT_AS_LONGLONG(a) | (SIMD_INT_AS_LONGLONG(a) >> 32)
+
+#define SIMD_LL_SET2_EPI32(a, b) \
+    SIMD_INT_AS_LONGLONG(a) | (SIMD_INT_AS_LONGLONG(b) >> 32)
+
+#endif//__GNUC__
 
 #ifdef SIMD_SSE2_ENABLE
+
     namespace Sse2
     {
         SIMD_INLINE __m128i _mm_set2_epi8(char a0, char a1)
         {
             return _mm_unpacklo_epi8(_mm_set1_epi8(a0), _mm_set1_epi8(a1));
         }
-        
+
         SIMD_INLINE __m128i _mm_set2_epi16(short a0, short a1)
         {
             return _mm_unpacklo_epi16(_mm_set1_epi16(a0), _mm_set1_epi16(a1));
@@ -68,40 +105,14 @@ namespace Simd
 		}
 	}
 
-#if defined(_M_X64) || defined(__GNUC__)
-
-#define SIMD_MM_SET1_EPI8(a) \
-    _mm_set1_epi8((char)(a))
-
-#define SIMD_MM_SET2_EPI8(a0, a1) \
-    Simd::Sse2::_mm_set2_epi8((char)(a0), (char)(a1))
-    
-#define SIMD_MM_SET1_EPI16(a) \
-    _mm_set1_epi16((short)(a))
-    
-#define SIMD_MM_SET2_EPI16(a0, a1) \
-    Simd::Sse2::_mm_set2_epi16((short)(a0), (short)(a1))
-
-#define SIMD_MM_SET1_EPI32(a) \
-    _mm_set1_epi32((int)(a))
-    
-#define SIMD_MM_SET2_EPI32(a0, a1) \
-    Simd::Sse2::_mm_set2_epi32((int)(a0), (int)(a1))
-
-#define SIMD_MM_SET1_PS(a) \
-	_mm_set_ps1((float)(a))
-
-#define SIMD_MM_SET2_PS(a0, a1) \
-	Simd::Sse2::_mm_set2_ps((float)(a0), (float)(a1))
-
-#else //defined(_M_X64) || defined(__GNUC__)
+#if defined(_MSC_VER)
 
 #define SIMD_MM_SET1_EPI8(a) \
     {SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), \
     SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), \
     SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), \
     SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a)}
-    
+
 #define SIMD_MM_SET2_EPI8(a0, a1) \
     {SIMD_AS_CHAR(a0), SIMD_AS_CHAR(a1), SIMD_AS_CHAR(a0), SIMD_AS_CHAR(a1), \
     SIMD_AS_CHAR(a0), SIMD_AS_CHAR(a1), SIMD_AS_CHAR(a0), SIMD_AS_CHAR(a1), \
@@ -122,17 +133,38 @@ namespace Simd
 #define SIMD_MM_SET2_EPI32(a0, a1) \
     {SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1), SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1)}
 
+#elif defined(__GNUC__)
+
+#define SIMD_MM_SET1_EPI8(a) \
+    {SIMD_LL_SET1_EPI8(a), SIMD_LL_SET1_EPI8(a)}
+
+#define SIMD_MM_SET2_EPI8(a0, a1) \
+    {SIMD_LL_SET2_EPI8(a0, a1), SIMD_LL_SET2_EPI8(a0, a1)}
+
+#define SIMD_MM_SET1_EPI16(a) \
+    {SIMD_LL_SET1_EPI16(a), SIMD_LL_SET1_EPI16(a)}
+
+#define SIMD_MM_SET2_EPI16(a0, a1) \
+    {SIMD_LL_SET2_EPI16(a0, a1), SIMD_LL_SET2_EPI16(a0, a1)}
+
+#define SIMD_MM_SET1_EPI32(a) \
+    {SIMD_LL_SET1_EPI32(a), SIMD_LL_SET1_EPI32(a)}
+
+#define SIMD_MM_SET2_EPI32(a0, a1) \
+    {SIMD_LL_SET2_EPI32(a0, a1), SIMD_LL_SET2_EPI32(a0, a1)}
+
+#endif// defined(_MSC_VER) || defined(__GNUC__)
+
 #define SIMD_MM_SET1_PS(a) \
 	{float(a), float(a), float(a), float(a)}
 
 #define SIMD_MM_SET2_PS(a0, a1) \
 	{float(a0), float(a1), float(a0), float(a1)}
 
-#endif //defined(_M_X64) || defined(__GNUC__)
-
 #endif// SIMD_SSE2_ENABLE
 
 #ifdef SIMD_AVX2_ENABLE
+
 	namespace Avx2
 	{
 		SIMD_INLINE __m256i _mm256_set2_epi8(char a0, char a1)
@@ -156,33 +188,7 @@ namespace Simd
 		}
 	}
 
-#if defined(__GNUC__)
-
-#define SIMD_MM256_SET1_EPI8(a) \
-	_mm256_set1_epi8((char)(a))
-
-#define SIMD_MM256_SET2_EPI8(a0, a1) \
-	Simd::Avx2::_mm256_set2_epi8((char)(a0), (char)(a1))
-
-#define SIMD_MM256_SET1_EPI16(a) \
-	_mm256_set1_epi16((short)(a))
-
-#define SIMD_MM256_SET2_EPI16(a0, a1) \
-	Simd::Avx2::_mm256_set2_epi16((short)(a0), (short)(a1))
-
-#define SIMD_MM256_SET1_EPI32(a) \
-	_mm256_set1_epi32((int)(a))
-
-#define SIMD_MM256_SET2_EPI32(a0, a1) \
-	Simd::Avx2::_mm256_set2_epi32((int)(a0), (int)(a1))
-
-#define SIMD_MM256_SET1_PS(a) \
-	_mm256_set1_ps((float)(a))
-
-#define SIMD_MM256_SET2_PS(a0, a1) \
-	Simd::Avx2::_mm256_set2_ps((float)(a0), (float)(a1))
-
-#else //defined(__GNUC__)
+#if defined(_MSC_VER)
 
 #define SIMD_MM256_SET1_EPI8(a) \
 	{SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), SIMD_AS_CHAR(a), \
@@ -224,6 +230,34 @@ namespace Simd
 	{SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1), SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1), \
 	SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1), SIMD_AS_4CHARS(a0), SIMD_AS_4CHARS(a1)}
 
+#elif defined(__GNUC__)
+
+#define SIMD_MM256_SET1_EPI8(a) \
+    {SIMD_LL_SET1_EPI8(a), SIMD_LL_SET1_EPI8(a), \
+    SIMD_LL_SET1_EPI8(a), SIMD_LL_SET1_EPI8(a)}
+
+#define SIMD_MM256_SET2_EPI8(a0, a1) \
+    {SIMD_LL_SET2_EPI8(a0, a1), SIMD_LL_SET2_EPI8(a0, a1), \
+    SIMD_LL_SET2_EPI8(a0, a1), SIMD_LL_SET2_EPI8(a0, a1)}
+
+#define SIMD_MM256_SET1_EPI16(a) \
+    {SIMD_LL_SET1_EPI16(a), SIMD_LL_SET1_EPI16(a), \
+    SIMD_LL_SET1_EPI16(a), SIMD_LL_SET1_EPI16(a)}
+
+#define SIMD_MM256_SET2_EPI16(a0, a1) \
+    {SIMD_LL_SET2_EPI16(a0, a1), SIMD_LL_SET2_EPI16(a0, a1), \
+    SIMD_LL_SET2_EPI16(a0, a1), SIMD_LL_SET2_EPI16(a0, a1)}
+
+#define SIMD_MM256_SET1_EPI32(a) \
+    {SIMD_LL_SET1_EPI32(a), SIMD_LL_SET1_EPI32(a), \
+    SIMD_LL_SET1_EPI32(a), SIMD_LL_SET1_EPI32(a)}
+
+#define SIMD_MM256_SET2_EPI32(a0, a1) \
+    {SIMD_LL_SET2_EPI32(a0, a1), SIMD_LL_SET2_EPI32(a0, a1), \
+    SIMD_LL_SET2_EPI32(a0, a1), SIMD_LL_SET2_EPI32(a0, a1)}
+
+#endif// defined(_MSC_VER) || defined(__GNUC__)
+
 #define SIMD_MM256_SET1_PS(a) \
 	{float(a), float(a), float(a), float(a), \
 	float(a), float(a), float(a), float(a)}
@@ -231,8 +265,6 @@ namespace Simd
 #define SIMD_MM256_SET2_PS(a0, a1) \
 	{float(a0), float(a1), float(a0), float(a1), \
 	float(a0), float(a1), float(a0), float(a1)}
-
-#endif// defined(__GNUC__)
 
 #endif// SIMD_AVX2_ENABLE
 }
