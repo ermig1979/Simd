437574468c77d224206eae83b45e887526d75b0f
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2013-06-24 12:41:27 +0000

    +added function StretchGray2x2;

---
+++ src/Simd/SimdStretchGray2x2.cpp
@@ -0,0 +1,126 @@
+/*
+* Simd Library.
+*
+* Copyright (c) 2011-2013 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in 
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "Simd/SimdEnable.h"
+#include "Simd/SimdMemory.h"
+#include "Simd/SimdConst.h"
+#include "Simd/SimdStretchGray2x2.h"
+
+namespace Simd
+{
+    namespace Base
+    {
+        void StretchGray2x2(const uchar *src, size_t srcWidth, size_t srcHeight, size_t srcStride, 
+            uchar *dst, size_t dstWidth, size_t dstHeight, size_t dstStride)
+        {
+            assert(srcWidth*2 == dstWidth && srcHeight*2 == dstHeight);
+
+			for(size_t row = 0; row < srcHeight; ++row)
+			{
+				uchar * dstEven = dst;
+				uchar * dstOdd = dst + dstStride;
+				for(size_t srcCol = 0; srcCol < srcWidth; srcCol += 1, dstEven += 2, dstOdd += 2)
+				{
+					uchar value = src[srcCol];
+					dstEven[0] = value;
+					dstEven[1] = value;
+					dstOdd[0] = value;
+					dstOdd[1] = value;
+				}
+				src += srcStride;
+				dst += 2*dstStride;
+			}
+        }
+    }
+
+#ifdef SIMD_SSE2_ENABLE    
+    namespace Sse2
+    {
+		template<bool align> SIMD_INLINE void StoreUnpacked(__m128i value, uchar * dst)
+		{
+			Store<align>((__m128i*)(dst + 0), _mm_unpacklo_epi8(value, value));
+			Store<align>((__m128i*)(dst + A), _mm_unpackhi_epi8(value, value));
+		}
+
+		template <bool align> void StretchGray2x2(
+			const uchar *src, size_t srcWidth, size_t srcHeight, size_t srcStride, 
+			uchar *dst, size_t dstWidth, size_t dstHeight, size_t dstStride)
+		{
+			assert(srcWidth*2 == dstWidth && srcHeight*2 == dstHeight && srcWidth >= A);
+			if(align)
+			{
+				assert(Aligned(src) && Aligned(srcStride));
+				assert(Aligned(dst) && Aligned(dstStride));
+			}
+
+			size_t alignedWidth = AlignLo(srcWidth, A);
+			for(size_t row = 0; row < srcHeight; ++row)
+			{
+				uchar * dstEven = dst;
+				uchar * dstOdd = dst + dstStride;
+				for(size_t srcCol = 0, dstCol = 0; srcCol < alignedWidth; srcCol += A, dstCol += DA)
+				{
+					__m128i value = Load<align>((__m128i*)(src + srcCol));
+					StoreUnpacked<align>(value, dstEven + dstCol);
+					StoreUnpacked<align>(value, dstOdd + dstCol);
+				}
+				if(alignedWidth != srcWidth)
+				{
+					__m128i value = Load<false>((__m128i*)(src + srcWidth - A));
+					StoreUnpacked<false>(value, dstEven + dstWidth - 2*A);
+					StoreUnpacked<false>(value, dstOdd + dstWidth - 2*A);
+				}
+				src += srcStride;
+				dst += 2*dstStride;
+			}
+		}
+
+		void StretchGray2x2(const uchar *src, size_t srcWidth, size_t srcHeight, size_t srcStride, 
+			uchar *dst, size_t dstWidth, size_t dstHeight, size_t dstStride)
+		{
+			if(Aligned(src) && Aligned(srcStride) && Aligned(dst) && Aligned(dstStride))
+				StretchGray2x2<true>(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride);
+			else
+				StretchGray2x2<false>(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride);
+		}
+    }
+#endif// SIMD_SSE2_ENABLE
+
+    void StretchGray2x2(const uchar *src, size_t srcWidth, size_t srcHeight, size_t srcStride, 
+        uchar *dst, size_t dstWidth, size_t dstHeight, size_t dstStride)
+    {
+#ifdef SIMD_SSE2_ENABLE
+        if(Sse2::Enable && srcWidth >= Sse2::A)
+            Sse2::StretchGray2x2(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride);
+        else
+#endif//SIMD_SSE2_ENABLE
+            Base::StretchGray2x2(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride);
+    }
+
+	void StretchGray2x2(const View & src, View & dst)
+	{
+		assert(src.format == View::Gray8 && dst.format == View::Gray8);
+
+		StretchGray2x2(src.data, src.width, src.height, src.stride, dst.data, dst.width, dst.height, dst.stride);
+	}
+}
---
+++ src/Test/TestStretchGray.cpp
@@ -0,0 +1,84 @@
+/*
+* Simd Library Tests.
+*
+* Copyright (c) 2011-2013 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy 
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+* copies of the Software, and to permit persons to whom the Software is 
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in 
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+#include "Test/TestUtils.h"
+#include "Test/TestPerformance.h"
+
+namespace Test
+{
+	namespace
+	{
+		struct Func
+		{
+			typedef void (*FuncPtr)(const uchar *src, size_t srcWidth, size_t srcHeight, size_t srcStride, 
+				uchar *dst, size_t dstWidth, size_t dstHeight, size_t dstStride);
+
+			FuncPtr func;
+			std::string description;
+
+			Func(const FuncPtr & f, const std::string & d) : func(f), description(d) {}
+
+			void Call(const View & src, View & dst) const
+			{
+				TEST_PERFORMANCE_TEST(description);
+				func(src.data, src.width, src.height, src.stride, dst.data, dst.width, dst.height, dst.stride);
+			}
+		};
+	}
+
+#define FUNC(function) Func(function, #function)
+
+	bool StretchGrayTest(int width, int height, const Func & f1, const Func & f2)
+	{
+		bool result = true;
+
+		std::cout << "Test " << f1.description << " & " << f2.description << " [" << width << ", " << height << "]." << std::endl;
+
+		const int stretchedWidth = width*2;
+		const int stretchedHeight = height*2;
+
+		View s(width, height, View::Gray8, NULL, TEST_ALIGN(width));
+		FillRandom(s);
+
+		View d1(stretchedWidth, stretchedHeight, View::Gray8, NULL, TEST_ALIGN(stretchedWidth));
+		View d2(stretchedWidth, stretchedHeight, View::Gray8, NULL, TEST_ALIGN(stretchedWidth));
+
+		TEST_EXECUTE_AT_LEAST_MIN_TIME(f1.Call(s, d1));
+
+		TEST_EXECUTE_AT_LEAST_MIN_TIME(f2.Call(s, d2));
+
+		result = result && Compare(d1, d2, 0, true, 10);
+
+		return result;
+	}
+
+	bool StretchGray2x2Test()
+	{
+		bool result = true;
+
+		result = result && StretchGrayTest(W, H, FUNC(Simd::Base::StretchGray2x2), FUNC(Simd::StretchGray2x2));
+		result = result && StretchGrayTest(W - 1, H + 1, FUNC(Simd::Base::StretchGray2x2), FUNC(Simd::StretchGray2x2));
+
+		return result;
+	}
+}
