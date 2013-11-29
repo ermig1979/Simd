9863a3fa1f6ba235cae10a57f774ab7c3fb59994
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2013-11-29 08:19:59 +0000

    *fixed bug: wrong checking of AVX2 support for GCC;

--- trunk/src/Simd/SimdSse42Crc32.cpp
+++ trunk/src/Simd/SimdSse42Crc32.cpp
@@ -21,7 +21,6 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 */
-#include "Simd/SimdEnable.h"
 #include "Simd/SimdMemory.h"
 #include "Simd/SimdSse42.h"
 
