862d12a032e2b48ff669202d5989d97617570a79
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2014-03-18 09:39:24 +0000

    *optimized of AVX2 implementation of function BgrToGray.

--- trunk/src/Simd/SimdAvx2BgrToGray.cpp
+++ trunk/src/Simd/SimdAvx2BgrToGray.cpp
@@ -26,48 +26,79 @@
 #include "Simd/SimdConst.h"
 #include "Simd/SimdBase.h"
 #include "Simd/SimdAvx2.h"
+#include "Simd/SimdStore.h"
+#include "Simd/SimdLoad.h"
 
 namespace Simd
 {
 #ifdef SIMD_AVX2_ENABLE    
     namespace Avx2
     {
-        namespace
+        const __m256i K16_BLUE_RED = SIMD_MM256_SET2_EPI16(Base::BLUE_TO_GRAY_WEIGHT, Base::RED_TO_GRAY_WEIGHT);        
+        const __m256i K16_GREEN_ROUND = SIMD_MM256_SET2_EPI16(Base::GREEN_TO_GRAY_WEIGHT, Base::BGR_TO_GRAY_ROUND_TERM);
+
+        SIMD_INLINE __m256i BgraToGray32(__m256i bgra)
         {
-            struct Buffer
-            {
-                Buffer(size_t width)
-                {
-                    _p = Allocate(sizeof(uint8_t)*4*width);
-                    bgra = (uint8_t*)_p;
-                }
-
-                ~Buffer()
-                {
-                    Free(_p);
-                }
-
-                uint8_t * bgra;
-            private:
-                void *_p;
-            };	
+            const __m256i g0a0 = _mm256_and_si256(_mm256_srli_si256(bgra, 1), K16_00FF);
+            const __m256i b0r0 = _mm256_and_si256(bgra, K16_00FF);
+            const __m256i weightedSum = _mm256_add_epi32(_mm256_madd_epi16(g0a0, K16_GREEN_ROUND), _mm256_madd_epi16(b0r0, K16_BLUE_RED));
+            return _mm256_srli_epi32(weightedSum, Base::BGR_TO_GRAY_AVERAGING_SHIFT);
         }
 
-        void BgrToGray(const uint8_t *bgr, size_t width, size_t height, size_t bgrStride, uint8_t *gray, size_t grayStride)
+        SIMD_INLINE __m256i BgraToGray(__m256i bgra[4])
+        {
+            const __m256i lo = PackI32ToI16(BgraToGray32(bgra[0]), BgraToGray32(bgra[1]));
+            const __m256i hi = PackI32ToI16(BgraToGray32(bgra[2]), BgraToGray32(bgra[3]));
+            return PackU16ToU8(lo, hi);
+        }
+
+        __m256i PermuteAndShiffle(__m256i bgr, __m256i permute, __m256i shuffle)
+        {
+            return _mm256_shuffle_epi8(_mm256_permutevar8x32_epi32(bgr, permute), shuffle);
+        }
+
+        template <bool align> SIMD_INLINE __m256i BgrToGray(const uint8_t * bgr, __m256i permuteBody, __m256i permuteTail, __m256i shuffle)
+        {
+            __m256i bgra[4];
+            bgra[0] = _mm256_or_si256(K32_01000000, PermuteAndShiffle(Load<align>((__m256i*)(bgr + 0)), permuteBody, shuffle));
+            bgra[1] = _mm256_or_si256(K32_01000000, PermuteAndShiffle(Load<false>((__m256i*)(bgr + 24)), permuteBody, shuffle));
+            bgra[2] = _mm256_or_si256(K32_01000000, PermuteAndShiffle(Load<false>((__m256i*)(bgr + 48)), permuteBody, shuffle));
+            bgra[3] = _mm256_or_si256(K32_01000000, PermuteAndShiffle(Load<align>((__m256i*)(bgr + 64)), permuteTail, shuffle));
+            return BgraToGray(bgra);
+        }
+
+        template <bool align> void BgrToGray(const uint8_t * bgr, size_t width, size_t height, size_t bgrStride, uint8_t * gray, size_t grayStride)
         {
             assert(width >= A);
+            if(align)
+                assert(Aligned(gray) && Aligned(grayStride) && Aligned(bgr) && Aligned(bgrStride));
 
-            Buffer buffer(width);
+            size_t alignedWidth = AlignLo(width, A);
 
-            for(size_t row = 1; row < height; ++row)
+            __m256i _permuteBody = _mm256_setr_epi32(0, 1, 2, 0, 3, 4, 5, 0);
+            __m256i _permuteTail = _mm256_setr_epi32(2, 3, 4, 0, 5, 6, 7, 0);
+
+            __m256i _shuffle = _mm256_setr_epi8(
+                0x0, 0x1, 0x2, -1, 0x3, 0x4, 0x5, -1, 0x6, 0x7, 0x8, -1, 0x9, 0xA, 0xB, -1,
+                0x0, 0x1, 0x2, -1, 0x3, 0x4, 0x5, -1, 0x6, 0x7, 0x8, -1, 0x9, 0xA, 0xB, -1);
+
+            for(size_t row = 0; row < height; ++row)
             {
-                Base::BgrToBgra(bgr, width, buffer.bgra, false, false, 0xFF);
-                Avx2::BgraToGray(buffer.bgra, width, 1, 4*width, gray, width);
+                for(size_t col = 0; col < alignedWidth; col += A)
+                    Store<align>((__m256i*)(gray + col), BgrToGray<align>(bgr + 3*col, _permuteBody, _permuteTail, _shuffle));
+                if(width != alignedWidth)
+                    Store<false>((__m256i*)(gray + width - A), BgrToGray<false>(bgr + 3*(width - A), _permuteBody, _permuteTail, _shuffle));
                 bgr += bgrStride;
                 gray += grayStride;
             }
-            Base::BgrToBgra(bgr, width, buffer.bgra, false, true, 0xFF);
-            Avx2::BgraToGray(buffer.bgra, width, 1, 4*width, gray, width);
+        }
+
+        void BgrToGray(const uint8_t * bgr, size_t width, size_t height, size_t bgrStride, uint8_t * gray, size_t grayStride)
+        {
+            if(Aligned(gray) && Aligned(grayStride) && Aligned(bgr) && Aligned(bgrStride))
+                BgrToGray<true>(bgr, width, height, bgrStride, gray, grayStride);
+            else
+                BgrToGray<false>(bgr, width, height, bgrStride, gray, grayStride);
         }
     }
 #endif//SIMD_Avx2_ENABLE
