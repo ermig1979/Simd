2489bbef6bd05acb60556e111fc45822655fbd2d
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2014-03-18 12:02:46 +0000

    +added AVX2 optimization of function BgrToBgra.

--- src/Simd/SimdAvx2BgrToGray.cpp
+++ src/Simd/SimdAvx2BgrToGray.cpp
@@ -52,7 +52,7 @@ namespace Simd
             return PackU16ToU8(lo, hi);
         }
 
-        __m256i PermuteAndShiffle(__m256i bgr, __m256i permute, __m256i shuffle)
+        SIMD_INLINE __m256i PermuteAndShiffle(__m256i bgr, __m256i permute, __m256i shuffle)
         {
             return _mm256_shuffle_epi8(_mm256_permutevar8x32_epi32(bgr, permute), shuffle);
         }
