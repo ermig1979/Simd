0e5985f875d161d04c542a89d2590f43f517fe95
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2014-05-14 11:25:36 +0000

    +added CMakeLists.txt for GCC.

--- prj/cmd/GetVersion.cmd
+++ prj/cmd/GetVersion.cmd
@@ -4,14 +4,11 @@ echo Try to estimate SVN revision:
 
 set SUBWCREV_EXE=SubWCRev.exe
 set TRUNK_DIR=..\..
-set USER_VERSION_TXT=%TRUNK_DIR%\prj\txt\Version.txt
+set USER_VERSION_TXT=%TRUNK_DIR%\prj\txt\UserVersion.txt
+set FULL_VERSION_TXT=%TRUNK_DIR%\prj\txt\FullVersion.txt
 set SIMD_VERSION_H_TXT=%TRUNK_DIR%\prj\txt\SimdVersion.h.txt
 set SIMD_VERSION_H=%TRUNK_DIR%\src\Simd\SimdVersion.h
-set VERSION_CMD=%TRUNK_DIR%\prj\cmd\Version.cmd
 set FIND_AND_REPLACE_CMD=%TRUNK_DIR%\prj\cmd\FindAndReplace.cmd
-
-set USER_VERSION=
-for /f "delims=" %%i in ('type %USER_VERSION_TXT%') do set USER_VERSION=%%i
 
 for %%X in (%SUBWCREV_EXE%) do (set SUBWCREV_EXE_FOUND=%%~$PATH:X)
 if not defined SUBWCREV_EXE_FOUND (
@@ -22,25 +19,25 @@ exit 0
 %SUBWCREV_EXE% %TRUNK_DIR%
 if ERRORLEVEL 1 exit 0
 
-if exist %VERSION_CMD% (
-    call %VERSION_CMD%
+set /p USER_VERSION=<%USER_VERSION_TXT%
+
+if exist %FULL_VERSION_TXT% (
+set /p FULL_VERSION=<%FULL_VERSION_TXT%
 ) else (
-	set VERSION=0
+set FULL_VERSION=0.0.0.0
 )
-set LAST_VERSION=%VERSION%
+set LAST_VERSION=%FULL_VERSION%
 
-echo set VERSION=%USER_VERSION%.$WCREV$>%VERSION_CMD%
-%SUBWCREV_EXE% %TRUNK_DIR% %VERSION_CMD% %VERSION_CMD%
-call %VERSION_CMD%
+echo %USER_VERSION%.$WCREV$>%FULL_VERSION_TXT%
+%SUBWCREV_EXE% %TRUNK_DIR% %FULL_VERSION_TXT% %FULL_VERSION_TXT%
+set /p FULL_VERSION=<%FULL_VERSION_TXT%
 
-if not %LAST_VERSION% == %VERSION% set NEED_TO_UPDATE=1
+if not %LAST_VERSION% == %FULL_VERSION% set NEED_TO_UPDATE=1
 if not exist %SIMD_VERSION_H% set NEED_TO_UPDATE=1
 
 if not defined NEED_TO_UPDATE (
 echo Skip updating of file "%SIMD_VERSION_H%" because there are not any changes.
-exit 0
-)
-
+) else (
 echo Create or update file "%SIMD_VERSION_H%".
-
-call %FIND_AND_REPLACE_CMD% @VERSION@ %VERSION% %SIMD_VERSION_H_TXT%>%SIMD_VERSION_H%
+call %FIND_AND_REPLACE_CMD% @VERSION@ %FULL_VERSION% %SIMD_VERSION_H_TXT%>%SIMD_VERSION_H%
+)
---
+++ prj/sh/GetVersion.sh
@@ -0,0 +1,46 @@
+echo "Try to estimate SVN revision:"
+
+TRUNK_DIR="../.."
+USER_VERSION_TXT="$TRUNK_DIR/prj/txt/UserVersion.txt"
+FULL_VERSION_TXT="$TRUNK_DIR/prj/txt/FullVersion.txt"
+SIMD_VERSION_H="$TRUNK_DIR/src/Simd/SimdVersion.h"
+SIMD_VERSION_H_TXT="$TRUNK_DIR/prj/txt/SimdVersion.h.txt"
+
+if [ -e "$FULL_VERSION_TXT" ]
+then
+	LAST_VERSION=`cat $FULL_VERSION_TXT`
+else
+	LAST_VERSION="0.0.0.0"
+fi
+
+cp $USER_VERSION_TXT $FULL_VERSION_TXT
+printf . >>$FULL_VERSION_TXT
+svn info $TRUNK_DIR | grep Revision: | cut -c11->>$FULL_VERSION_TXT
+FULL_VERSION=`cat $FULL_VERSION_TXT`
+
+NEED_TO_UPDATE="0"
+if [ "$LAST_VERSION" = "$FULL_VERSION" ] 
+then
+	echo "Last project version '$LAST_VERSION' is equal to current version '$FULL_VERSION'."
+else
+	echo "Last project version '$LAST_VERSION' is not equal to current version '$FULL_VERSION'."
+	NEED_TO_UPDATE="1"
+fi
+
+if [ -e "$SIMD_VERSION_H" ] 
+then
+	echo "File '$SIMD_VERSION_H' is already exist."
+else
+	echo "File '$SIMD_VERSION_H' is not exist."
+	NEED_TO_UPDATE="1"
+fi
+
+if [ "$NEED_TO_UPDATE" = "0" ] 
+then
+	echo "Skip updating of '$SIMD_VERSION_H' file."
+else
+	echo "Create or update '$SIMD_VERSION_H' file."
+	cp $SIMD_VERSION_H_TXT $SIMD_VERSION_H
+	sed "-i" "s/@VERSION@/$FULL_VERSION/g" $SIMD_VERSION_H
+fi
+
