a763810bfd92240948f8a6107472fa1c1e0be567
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2014-07-10 06:30:33 +0000

    +added cross platform tests and VSX optimization of function GrayToBgr.

--- trunk/src/Simd/SimdVsx.h
+++ trunk/src/Simd/SimdVsx.h
@@ -137,6 +137,8 @@ namespace Simd
         void GaussianBlur3x3(const uint8_t * src, size_t srcStride, size_t width, size_t height, 
             size_t channelCount, uint8_t * dst, size_t dstStride);
 
+        void GrayToBgr(const uint8_t *gray, size_t width, size_t height, size_t grayStride, uint8_t *bgr, size_t bgrStride);
+
         void GrayToBgra(const uint8_t * gray, size_t width, size_t height, size_t grayStride, uint8_t * bgra, size_t bgraStride, uint8_t alpha);
 
         void AbsSecondDerivativeHistogram(const uint8_t *src, size_t width, size_t height, size_t stride,
