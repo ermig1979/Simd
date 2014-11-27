b84014e29258c9a511ebd1b6962a0d0f713bd7ed
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2014-11-27 12:57:20 +0000

    *fixed compiler error in function BgrToYuv444p (PowerPC, Debug mode).

--- src/Simd/SimdVsxBgrToYuv.cpp
+++ src/Simd/SimdVsxBgrToYuv.cpp
@@ -135,7 +135,7 @@ namespace Simd
         template <bool align> void BgrToYuv444p(const uint8_t * bgr, size_t width, size_t height, size_t bgrStride, uint8_t * y, size_t yStride,
             uint8_t * u, size_t uStride, uint8_t * v, size_t vStride)
         {
-            assert(width >= D);
+            assert(width >= A);
             if(align)
             {
                 assert(Aligned(y) && Aligned(yStride) && Aligned(u) &&  Aligned(uStride));
