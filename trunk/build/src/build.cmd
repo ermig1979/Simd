25422b43ab509defbc594624fd079e0e14e92cb6
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2015-01-05 08:19:29 +0000

    *changed BuildZip.cmd location.

--- trunk/build/src/build.cmd
+++ trunk/build/src/build.cmd
@@ -6,10 +6,10 @@ echo Execution file "%RAR_EXE%" is to exists!
 exit 1
 )
 
-call GetVersion.cmd
+call ..\..\prj\cmd\GetVersion.cmd
 
-set OUT_DIR=%TRUNK_DIR%\build
-set TMP_DIR=%TRUNK_DIR%\build\simd
+set OUT_DIR=%TRUNK_DIR%\build\out
+set TMP_DIR=%TRUNK_DIR%\build\out\simd
 
 if not exist %OUT_DIR% mkdir %OUT_DIR%
 
@@ -37,11 +37,17 @@ erase %TMP_DIR%\prj\*.layout /q /s /f
 erase %TMP_DIR%\prj\*.cbTemp /q /s /f
 erase %TMP_DIR%\prj\*.pdb /q /s /f
 
+erase %TMP_DIR%\prj\txt\UserConfig.txt /q /s /f
+erase %TMP_DIR%\prj\txt\FullConfig.txt /q /s /f
 erase %TMP_DIR%\prj\cmd\*.txt /q /s /f
 erase %TMP_DIR%\prj\cmd\*.lnk /q /s /f
+erase %TMP_DIR%\prj\cmd\BuildZip.cmd /q /s /f
 erase %TMP_DIR%\prj\cmd\BuildSite.cmd /q /s /f
+erase %TMP_DIR%\prj\cmd\BuildHelp.cmd /q /s /f
 erase %TMP_DIR%\doc\help /q /s /f
 rmdir %TMP_DIR%\doc\help /q /s
+erase %TMP_DIR%\doc\src /q /s /f
+rmdir %TMP_DIR%\doc\src /q /s
 
 %RAR_EXE% a -afzip -ep1 -r %OUT_DIR%\simd.%FULL_VERSION%.zip %TMP_DIR%
 
