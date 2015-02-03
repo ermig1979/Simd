95583794b138a54afd452f51f54372d70e49db72
ermig1979 <ermig1979@202bb9e5-f92e-4852-8ce4-710ba47941df>
2015-02-03 09:01:38 +0000

    +added a description of Pixel::Bgr24, Pixel::Bgra32, Pixel::Hsv24 and Pixel::Hsl24 structures.

--- src/Simd/SimdPixel.hpp
+++ src/Simd/SimdPixel.hpp
@@ -28,6 +28,12 @@
 
 namespace Simd
 {
+    /*! @ingroup cpp_types
+
+        \short Contains various pixel structures.
+
+        Contains various pixel structures useful for work with Simd::View structure.
+    */
     namespace Pixel
     {
         struct Bgr24;
@@ -37,62 +43,258 @@ namespace Simd
 
         //-------------------------------------------------------------------------
 
+        /*!
+            \short 24-bit BGR pixel.
+
+            Provides manipulation of 24-bit BGR (Blue, Green, Red) pixels of the View struct.
+        */
         struct Bgr24
         {
-            uint8_t blue;
-            uint8_t green;
-            uint8_t red;
+            uint8_t blue; /*!< \brief 8-bit blue channel 24-bit BGR pixel. */
+            uint8_t green; /*!< \brief 8-bit green channel 24-bit BGR pixel. */
+            uint8_t red; /*!< \brief 8-bit red channel 24-bit BGR pixel. */
 
+            /*!
+                Creates a new 24-bit BGR pixel structure with specified channel values. 
+
+                \param [in] gray - initial value for all channels. It is equal to 0 by default.
+            */
             Bgr24(const uint8_t & gray = uint8_t(0));
+
+            /*!
+                Creates a new 24-bit BGR pixel structure with specified channel values. 
+
+                \param [in] b - initial value for blue channel.
+                \param [in] g - initial value for green channel.
+                \param [in] r - initial value for red channel.
+            */
             Bgr24(const uint8_t & b, const uint8_t & g, const uint8_t & r);
+
+            /*!
+                Creates a new 24-bit BGR pixel structure on the base of 32-bit BGRA pixel. 
+
+                \param [in] p - 32-bit BGRA pixel.
+            */
             Bgr24(const Bgra32 & p);
+
+            /*!
+                Creates a copy of 24-bit BGR pixel structure. 
+
+                \param [in] p - 24-bit BGR pixel.
+            */
             Bgr24(const Bgr24 & p);
 
+            /*!
+                \fn template <class A> static const Bgr24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets constant reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit BGR pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a constant reference to the pixel.
+            */
             template <class A> static const Bgr24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+            /*!
+                \fn template <class A> static Bgr24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit BGR pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a reference to the pixel.
+            */
             template <class A> static Bgr24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
         };
 
+        /*!
+            \short 32-bit BGRA pixel.
+
+            Provides manipulation of 32-bit BGRA (Blue, Green, Red, Alpha) pixels of the View struct.
+        */
         struct Bgra32
         {
-            uint8_t blue;
-            uint8_t green;
-            uint8_t red;
-            uint8_t alpha;
+            uint8_t blue; /*!< \brief 8-bit blue channel 32-bit BGRA pixel. */
+            uint8_t green; /*!< \brief 8-bit green channel 32-bit BGRA pixel. */
+            uint8_t red; /*!< \brief 8-bit red channel 32-bit BGRA pixel. */
+            uint8_t alpha; /*!< \brief 8-bit alpha channel 32-bit BGRA pixel. */
 
+            /*!
+                Creates a new 32-bit BGRA pixel structure with specified channel values. 
+
+                \param [in] gray - initial value for blue, green and red channels. It is equal to 0 by default.
+                \param [in] a - initial value for alpha channel. It is equal to 255 by default.
+            */
             Bgra32(const uint8_t & gray = uint8_t(0), const uint8_t & a = uint8_t(255));
+
+            /*!
+                Creates a new 32-bit BGRA pixel structure with specified channel values. 
+
+                \param [in] b - initial value for blue channel.
+                \param [in] g - initial value for green channel.
+                \param [in] r - initial value for red channel.
+                \param [in] a - initial value for alpha channel. It is equal to 255 by default.
+            */
             Bgra32(const uint8_t & b, const uint8_t & g, const uint8_t & r, const uint8_t & a = uint8_t(255));
+
+            /*!
+                Creates a new 32-bit BGRA pixel structure on the base of 32-bit BGR pixel. 
+
+                \param [in] p - 32-bit BGR pixel.
+                \param [in] a - initial value for alpha channel. It is equal to 255 by default.
+            */            
             Bgra32(const Bgr24 & p, const uint8_t & a = uint8_t(255));
+            
+            /*!
+                Creates a copy of 32-bit BGRA pixel structure. 
+
+                \param [in] p - 32-bit BGRA pixel.
+            */
             Bgra32(const Bgra32 & p);
 
+            /*!
+                \fn template <class A> static const Bgr32 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets constant reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 32-bit BGRA pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a constant reference to the pixel.
+            */
             template <class A> static const Bgra32 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+            /*!
+                \fn template <class A> static Bgr32 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 32-bit BGRA pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a reference to the pixel.
+            */
             template <class A> static Bgra32 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
         };
 
+        /*!
+            \short 24-bit HSV pixel.
+
+            Provides manipulation of 24-bit HSV (Hue, Saturation, Value) pixels of the View struct.
+        */
         struct Hsv24
         {
-            uint8_t hue;
-            uint8_t saturation;
-            uint8_t value;
+            uint8_t hue; /*!< \brief 8-bit hue channel 24-bit HSV pixel. */
+            uint8_t saturation; /*!< \brief 8-bit saturation channel 24-bit HSV pixel. */
+            uint8_t value; /*!< \brief 8-bit value channel 24-bit HSV pixel. */
 
+            /*!
+                Creates a new 24-bit HSV pixel structure with specified channel values. 
+
+                \param [in] gray - initial value for value channel. It is equal to 0 by default.
+            */
             Hsv24(const uint8_t & gray = uint8_t(0));
+
+            /*!
+                Creates a new 24-bit HSV pixel structure with specified channel values. 
+
+                \param [in] h - initial value for hue channel.
+                \param [in] s - initial value for saturation channel.
+                \param [in] v - initial value for value channel.
+            */            
             Hsv24(const uint8_t & h, const uint8_t & s, const uint8_t & v);
+
+            /*!
+                Creates a copy of 24-bit HSV pixel structure. 
+
+                \param [in] p - 24-bit HSV pixel.
+            */
             Hsv24(const Hsv24 & p);
 
+            /*!
+                \fn template <class A> static const Hsv24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets constant reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit HSV pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a constant reference to the pixel.
+            */
             template <class A> static const Hsv24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+            /*!
+                \fn template <class A> static Hsv24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit HSV pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a reference to the pixel.
+            */
             template <class A> static Hsv24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
         };
 
+        /*!
+            \short 24-bit HSL pixel.
+
+            Provides manipulation of 24-bit HSL (Hue, Saturation, Lightness) pixels of the View struct.
+        */
         struct Hsl24
         {
-            uint8_t hue;
-            uint8_t saturation;
-            uint8_t lightness;
+            uint8_t hue; /*!< \brief 8-bit hue channel 24-bit HSL pixel. */
+            uint8_t saturation; /*!< \brief 8-bit saturation channel 24-bit HSL pixel. */
+            uint8_t lightness; /*!< \brief 8-bit lightness channel 24-bit HSL pixel. */
 
+            /*!
+                Creates a new 24-bit HSL pixel structure with specified channel values. 
+
+                \param [in] gray - initial value for value channel. It is equal to 0 by default.
+            */
             Hsl24(const uint8_t & gray = uint8_t(0));
+
+            /*!
+                Creates a new 24-bit HSL pixel structure with specified channel values. 
+
+                \param [in] h - initial value for hue channel.
+                \param [in] s - initial value for saturation channel.
+                \param [in] l - initial value for lightness channel.
+            */            
             Hsl24(const uint8_t & h, const uint8_t & s, const uint8_t & l);
+
+            /*!
+                Creates a copy of 24-bit HSL pixel structure. 
+
+                \param [in] p - 24-bit HSL pixel.
+            */
             Hsl24(const Hsl24 & p);
 
+
+            /*!
+                \fn template <class A> static const Hsl24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets constant reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit HSL pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a constant reference to the pixel.
+            */
             template <class A> static const Hsl24 & At(const View<A> & view, ptrdiff_t col, ptrdiff_t row);
+            
+            /*!
+                \fn template <class A> static Hsl24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
+
+                Gets reference to the pixel with specific coordinates at the image view. 
+
+                \param [in] view - an image view of 24-bit HSL pixel format.
+                \param [in] col - x-coordinate of the pixel.
+                \param [in] row - y-coordinate of the pixel.
+                \return a reference to the pixel.
+            */
             template <class A> static Hsl24 & At(View<A> & view, ptrdiff_t col, ptrdiff_t row);
         };
 
