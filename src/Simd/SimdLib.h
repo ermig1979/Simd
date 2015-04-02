92a8470510e6db049b350be0842e4702e7896655
ermig1979 <ermig@tut.by>
2015-04-02 09:01:08 +0300

    *RELEASE 2.2.28.

--- src/Simd/SimdLib.h
+++ src/Simd/SimdLib.h
@@ -2272,7 +2272,7 @@ extern "C"
 
     /*! @ingroup shifting
 
-        \fn void SimdShiftBilinear(const uint8_t * src, size_t srcStride, size_t width, size_t height, size_t channelCount, const uint8_t * bkg, size_t bkgStride, double shiftX, double shiftY, size_t cropLeft, size_t cropTop, size_t cropRight, size_t cropBottom, uint8_t * dst, size_t dstStride);
+        \fn void SimdShiftBilinear(const uint8_t * src, size_t srcStride, size_t width, size_t height, size_t channelCount, const uint8_t * bkg, size_t bkgStride, const double * shiftX, const double * shiftY, size_t cropLeft, size_t cropTop, size_t cropRight, size_t cropBottom, uint8_t * dst, size_t dstStride);
 
         \short Performs shifting of input image with using bilinear interpolation. 
 
