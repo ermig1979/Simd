b7d3db8ba52a4b093acadcd4c64228642212ec1c
ermig1979 <ermig@tut.by>
2015-06-04 15:29:49 +0300

    *improving of SSSE3 and AVX2 optimizations of function ResizeBilinear for 8-bit gray images.

--- src/Simd/SimdSsse3ResizeBilinear.cpp
+++ src/Simd/SimdSsse3ResizeBilinear.cpp
@@ -58,6 +58,39 @@ namespace Simd
             private:
                 void *_p;
             };
+
+            struct Index
+            {
+                int src, dst;
+                uint8_t shuffle[A];
+            };
+
+            struct BufferG
+            {
+                BufferG(size_t width, size_t blocks, size_t height)
+                {
+                    _p = Allocate(3*width + sizeof(int)*2*height + blocks*sizeof(Index));
+                    bx[0] = (uint8_t*)_p;
+                    bx[1] = bx[0] + width;
+                    ax = bx[1] + width;
+                    ix = (Index*)(ax + width);
+                    iy = (int*)(ix + blocks);
+                    ay = iy + height;
+                }
+
+                ~BufferG()
+                {
+                    Free(_p);
+                }
+
+                uint8_t * bx[2];
+                uint8_t * ax;
+                Index * ix;
+                int * ay;
+                int * iy;
+            private:
+                void *_p;
+            };
         }
 
         template <size_t channelCount> void EstimateAlphaIndexX(size_t srcSize, size_t dstSize, int * indexes, uint8_t * alphas)
@@ -88,6 +121,57 @@ namespace Simd
                 for(size_t channel = 1; channel < channelCount; channel++)
                     ((uint16_t*)alphas)[channel] = *(uint16_t*)alphas;
                 alphas += 2*channelCount;
+            }
+        }
+
+        void EstimateAlphaIndexX(int srcSize, int dstSize, Index * indexes, uint8_t * alphas)
+        {
+            float scale = (float)srcSize/dstSize;
+
+            int srcNext = 0, dstNext = 0;
+            int block = -1, blockLast = std::max((int)::ceil(float(srcSize)/A), (int)::ceil(float(dstSize)/HA)) - 1;
+            for(int dstIndex = 0; dstIndex < dstSize; ++dstIndex)
+            {
+                float alpha = (float)((dstIndex + 0.5)*scale - 0.5);
+                int srcIndex = (int)::floor(alpha);
+                alpha -= srcIndex;
+
+                if(srcIndex < 0)
+                {
+                    srcIndex = 0;
+                    alpha = 0;
+                }
+
+                if(srcIndex > srcSize - 2)
+                {
+                    srcIndex = srcSize - 2;
+                    alpha = 1;
+                }
+
+                if((srcIndex >= srcNext || dstIndex >= dstNext) && block < blockLast)
+                {
+                    block++;
+                    indexes[block].src = srcIndex;
+                    indexes[block].dst = 2*dstIndex;
+                    if(block < blockLast)
+                    {
+                        srcNext += (int)A;
+                        dstNext += (int)HA;
+                    }
+                    else
+                    {
+                        srcNext = srcSize - (int)A;
+                        dstNext = dstSize - (int)HA;
+                    }
+                }
+                
+                int dst = 2*dstIndex - indexes[block].dst, src = srcIndex - indexes[block].src;
+                indexes[block].shuffle[dst] = src;
+                indexes[block].shuffle[dst + 1] = src + 1;
+
+                alphas[1] = (uint8_t)(alpha * Base::FRACTION_RANGE + 0.5);
+                alphas[0] = (uint8_t)(Base::FRACTION_RANGE - alphas[1]);
+                alphas += 2;
             }
         }
 
@@ -230,6 +314,69 @@ namespace Simd
             }
         }
 
+        SIMD_INLINE void LoadGray(const uint8_t * src, const Index & index, uint8_t * dst)
+        {
+            __m128i _src = _mm_loadu_si128((__m128i*)(src + index.src));
+            __m128i _shuffle = _mm_loadu_si128((__m128i*)&index.shuffle);
+            _mm_storeu_si128((__m128i*)(dst + index.dst), _mm_shuffle_epi8(_src, _shuffle));
+        }
+
+        void ResizeBilinearGray(const uint8_t *src, size_t srcWidth, size_t srcHeight, size_t srcStride, uint8_t *dst, size_t dstWidth, size_t dstHeight, size_t dstStride)
+        {
+            assert(dstWidth >= A);
+
+            size_t size = 2*dstWidth;
+            size_t bufferWidth = AlignHi(dstWidth, A)*2;
+            size_t blockCount = std::max((int)::ceil(float(srcWidth)/A), (int)::ceil(float(dstWidth)/HA));
+            size_t alignedSize = AlignHi(size, DA) - DA;
+
+            BufferG buffer(bufferWidth, blockCount, dstHeight);
+
+            Base::EstimateAlphaIndex(srcHeight, dstHeight, buffer.iy, buffer.ay, 1);
+
+            EstimateAlphaIndexX((int)srcWidth, (int)dstWidth, buffer.ix, buffer.ax);
+
+            ptrdiff_t previous = -2;
+
+            __m128i a[2];
+
+            for(size_t yDst = 0; yDst < dstHeight; yDst++, dst += dstStride)
+            {
+                a[0] = _mm_set1_epi16(int16_t(Base::FRACTION_RANGE - buffer.ay[yDst]));
+                a[1] = _mm_set1_epi16(int16_t(buffer.ay[yDst]));
+
+                ptrdiff_t sy = buffer.iy[yDst];
+                int k = 0;
+
+                if(sy == previous)
+                    k = 2;
+                else if(sy == previous + 1)
+                {
+                    Swap(buffer.bx[0], buffer.bx[1]);
+                    k = 1;
+                }
+
+                previous = sy;
+
+                for(; k < 2; k++)
+                {
+                    const uint8_t * psrc = src + (sy + k)*srcStride;
+                    uint8_t * pdst = buffer.bx[k];
+                    for(size_t i = 0; i < blockCount; ++i)
+                        LoadGray(psrc, buffer.ix[i], pdst);
+
+                    uint8_t * pbx = buffer.bx[k];
+                    for(size_t i = 0; i < bufferWidth; i += A)
+                        InterpolateX<1>((__m128i*)(buffer.ax + i), (__m128i*)(pbx + i));
+                }
+
+                for(size_t ib = 0, id = 0; ib < alignedSize; ib += DA, id += A)
+                    InterpolateY<true>(buffer.bx[0] + ib, buffer.bx[1] + ib, a, dst + id);
+                size_t i = size - DA;
+                InterpolateY<false>(buffer.bx[0] + i, buffer.bx[1] + i, a, dst + i/2);
+            }
+        }
+
         void ResizeBilinear(
             const uint8_t *src, size_t srcWidth, size_t srcHeight, size_t srcStride,
             uint8_t *dst, size_t dstWidth, size_t dstHeight, size_t dstStride, size_t channelCount)
@@ -237,7 +384,10 @@ namespace Simd
             switch(channelCount)
             {
             case 1: 
-                ResizeBilinear<1>(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride); 
+                if(srcWidth >= A && srcWidth < 4*dstWidth)
+                    ResizeBilinearGray(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride); 
+                else
+                    ResizeBilinear<1>(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride); 
                 break;
             case 2: 
                 ResizeBilinear<2>(src, srcWidth, srcHeight, srcStride, dst, dstWidth, dstHeight, dstStride); 
