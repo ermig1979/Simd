928d29d825cddc1202285ebb254b7ef2b990cd35
ermig1979 <ermig@tut.by>
2016-01-04 09:26:28 +0300

    *updated copyrights.

--- src/Test/TestBayerToBgra.cpp
+++ src/Test/TestBayerToBgra.cpp
@@ -1,7 +1,7 @@
 /*
 * Tests for Simd Library (http://simd.sourceforge.net).
 *
-* Copyright (c) 2011-2015 Yermalayeu Ihar.
+* Copyright (c) 2011-2016 Yermalayeu Ihar.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy 
 * of this software and associated documentation files (the "Software"), to deal
