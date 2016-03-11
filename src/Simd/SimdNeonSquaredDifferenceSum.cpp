cd60c79a77d7736a9e89b97c76580b406a50fbcd
ermig1979 <ermig@tut.by>
2016-03-11 14:53:10 +0300

    *improved NEON optimization of functions SquaredDifferenceSum and SquaredDifferenceSumMasked.

--- src/Simd/SimdNeonSquaredDifferenceSum.cpp
+++ src/Simd/SimdNeonSquaredDifferenceSum.cpp
@@ -31,18 +31,26 @@ namespace Simd
 #ifdef SIMD_NEON_ENABLE    
 	namespace Neon
     {
-		SIMD_INLINE uint16x8_t Square(uint8x8_t value)
+		SIMD_INLINE uint16x8_t Square(const uint8x8_t & value)
 		{
 			return vmull_u8(value, value);
 		}
 
-		SIMD_INLINE uint32x4_t SquaredDifferenceSum(uint8x16_t a, uint8x16_t b)
+		SIMD_INLINE uint32x4_t SquaredDifferenceSum(const uint8x16_t & a, const uint8x16_t & b)
 		{
 			uint8x16_t ad = vabdq_u8(a, b);
 			uint16x8_t lo = Square(vget_low_u8(ad));
 			uint16x8_t hi = Square(vget_high_u8(ad));
 			return vaddq_u32(vpaddlq_u16(lo), vpaddlq_u16(hi));
 		}
+
+        SIMD_INLINE uint32x4_t SquaredDifferenceSumMasked(const uint8x16_t & a, const uint8x16_t & b, const uint8x16_t & mask)
+        {
+            uint8x16_t ad = vandq_u8(vabdq_u8(a, b), mask);
+            uint16x8_t lo = Square(vget_low_u8(ad));
+            uint16x8_t hi = Square(vget_high_u8(ad));
+            return vaddq_u32(vpaddlq_u16(lo), vpaddlq_u16(hi));
+        }
 
 		template <bool align> void SquaredDifferenceSum(
 			const uint8_t * a, size_t aStride, const uint8_t * b, size_t bStride, 
@@ -67,9 +75,9 @@ namespace Simd
 				}
 				if(width - alignedWidth)
 				{
-					uint8x16_t _a = vandq_u8(tailMask, Load<align>(a + width - A));
-					uint8x16_t _b = vandq_u8(tailMask, Load<align>(b + width - A));
-					rowSum = vaddq_u32(rowSum, SquaredDifferenceSum(_a, _b));
+					uint8x16_t _a = Load<align>(a + width - A);
+					uint8x16_t _b = Load<align>(b + width - A);
+					rowSum = vaddq_u32(rowSum, SquaredDifferenceSumMasked(_a, _b, tailMask));
 				}
 				_sum = vaddq_u64(_sum, vpaddlq_u32(rowSum));
 				a += aStride;
@@ -108,16 +116,16 @@ namespace Simd
 				for (size_t col = 0; col < alignedWidth; col += A)
 				{
 					uint8x16_t _mask = vceqq_u8(Load<align>(mask + col), _index);
-					uint8x16_t _a = vandq_u8(_mask, Load<align>(a + col));
-					uint8x16_t _b = vandq_u8(_mask, Load<align>(b + col));
-					rowSum = vaddq_u32(rowSum, SquaredDifferenceSum(_a, _b));
+					uint8x16_t _a = Load<align>(a + col);
+					uint8x16_t _b = Load<align>(b + col);
+					rowSum = vaddq_u32(rowSum, SquaredDifferenceSumMasked(_a, _b, _mask));
 				}
 				if (width - alignedWidth)
 				{
 					uint8x16_t _mask = vandq_u8(tailMask, vceqq_u8(Load<align>(mask + width - A), _index));
-					uint8x16_t _a = vandq_u8(_mask, Load<align>(a + width - A));
-					uint8x16_t _b = vandq_u8(_mask, Load<align>(b + width - A));
-					rowSum = vaddq_u32(rowSum, SquaredDifferenceSum(_a, _b));
+					uint8x16_t _a = Load<align>(a + width - A);
+					uint8x16_t _b = Load<align>(b + width - A);
+					rowSum = vaddq_u32(rowSum, SquaredDifferenceSumMasked(_a, _b, _mask));
 				}
 				_sum = vaddq_u64(_sum, vpaddlq_u32(rowSum));
 				a += aStride;
