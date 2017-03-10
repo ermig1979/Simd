53b3eee92d31dfe678d68a53f8d0419b0538042f
ermig1979 <ermig@tut.by>
2017-03-10 09:42:26 +0300

    +added MotionDetector (part 3: added Texture).

--- src/Simd/SimdMotion.hpp
+++ src/Simd/SimdMotion.hpp
@@ -39,6 +39,7 @@ namespace Simd
         typedef Simd::Rectangle<ptrdiff_t> Rect;
         typedef Simd::View<Simd::Allocator> View; 
         typedef Simd::Frame<Simd::Allocator> Frame;
+        typedef Simd::Pyramid<Simd::Allocator> Pyramid;
 
         struct Position
         {
@@ -58,13 +59,18 @@ namespace Simd
         struct Metadata
         {
             Objects objects;
-
         };
 
         struct Model
         {
             Size size;
             Points roi;
+
+            Model(const Model & m)
+                : size(m.size)
+                , roi(m.roi)
+            {
+            }
 
             Model(const Size & s = Size(), const Points & r = Points())
                 : size(s)
@@ -83,7 +89,14 @@ namespace Simd
 
         struct Options
         {
+            int TextureGradientSaturation;
+            int TextureGradientBoost;
 
+            Options()
+            {
+                TextureGradientSaturation = 16;
+                TextureGradientBoost = 4;
+            }
         };
 
         struct Detector
@@ -104,11 +117,26 @@ namespace Simd
 
             bool SetModel(const Model & model)
             {
+                _model = model;
                 return true;
             }
 
             bool NextFrame(const Frame & input, Metadata & metadata, Frame * output = NULL)
             {
+                if (output && output->Size() != input.Size())
+                    return false;
+
+                if (_scene.model.size != input.Size())
+                {
+                    _model = Model(input.Size());
+                    if(!Calibrate(_model))
+                        return false;
+                }
+
+                SetFrame(input, output);
+
+                EstimateTextures();
+
                 return true;
             }
 
@@ -116,10 +144,109 @@ namespace Simd
             Options _options;
             Model _model;
 
+            struct Texture
+            {
+                struct Bound 
+                {
+                    Pyramid value; 
+                    Pyramid count; 
+
+                    void Create(const Size & size, int levelCount)
+                    {
+                        value.Recreate(size, levelCount);
+                        count.Recreate(size, levelCount);
+                    }
+                };
+
+                struct Feature 
+                {
+                    Pyramid value; 
+                    Bound lo; 
+                    Bound hi; 
+
+                    void Create(const Size & size, int levelCount)
+                    {
+                        value.Recreate(size, levelCount);
+                        lo.Create(size, levelCount);
+                        hi.Create(size, levelCount);
+                    }
+                };
+
+                enum FeatureType
+                {
+                    FeatureGray,
+                    FeatureDx,
+                    FeatureDy,
+                };
+
+                Feature gray; 
+                Feature dx; 
+                Feature dy; 
+
+                typedef std::vector<Feature *> Features;
+                Features features;
+
+                void Create(const Size & size, int levelCount)
+                {
+                    features.clear();
+                    gray.Create(size, levelCount);
+                    features.push_back(&gray);
+                    dx.Create(size, levelCount);
+                    features.push_back(&dx);
+                    dy.Create(size, levelCount);
+                    features.push_back(&dy);
+                }
+            };
+
             struct Scene
             {
+                Frame input, *output;
+                View gray;
+
                 Model model;
+
+                Texture texture;
+
+                void Create(const Model & m)
+                {
+                    model = m;
+                    gray.Recreate(model.size, View::Gray8);
+                    texture.Create(model.size, 3);
+                }
             };
+
+            bool SetFrame(const Frame & input, Frame * output)
+            {
+                _scene.input = input;
+                _scene.output = output;
+                Simd::Convert(input, Frame(_scene.gray).Ref());
+
+                return true;
+            }
+
+            bool Calibrate(const Model & model)
+            {
+                _scene.Create(model);
+
+                return true;
+            }
+
+            bool EstimateTextures()
+            {
+                Texture & texture = _scene.texture;
+
+                Simd::Copy(_scene.gray, texture.gray.value[0]);
+                Simd::Build(texture.gray.value, SimdReduce4x4);
+
+                for (size_t i = 0; i < texture.gray.value.Size(); ++i)
+                {
+                    Simd::TextureBoostedSaturatedGradient(texture.gray.value[i],
+                        _options.TextureGradientSaturation, _options.TextureGradientBoost, 
+                        texture.dx.value[i], texture.dy.value[i]);
+                }
+
+                return true;
+            }
 
             Scene _scene;
         };
