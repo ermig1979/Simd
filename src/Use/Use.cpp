4662afb79bdc601bea7133cd39d8fd9142a32650
ermig1979 <ermig@tut.by>
2017-05-04 13:53:44 +0300

    +added an example how to detect faces with using Simd::Detection on video captured by OpenCV.

---
+++ src/Use/Use.cpp
@@ -0,0 +1,90 @@
+/*
+* The use examples of Simd Library (http://ermig1979.github.io/Simd).
+*
+* Copyright (c) 2011-2017 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* In order to be enable of OpenCV examples for Visual Studio 2015 you have to rename file 'Ocv.prop.default' into 'Ocv.prop' and set there correct paths to OpenCV.
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Example
+{
+    typedef int(*Function)(int argc, char * argv[]);
+
+    std::string name;
+    Function func;
+
+    Example(const std::string & n, const Function & f)
+        : name(n)
+        , func(f)
+    {
+    }
+};
+typedef std::vector<Example> Examples;
+Examples g_examples;
+
+#ifdef SIMD_OPENCV_ENABLE
+#define USE_ADD_OPENCV_EXAMPLE(name) \
+    int Use##name(int argc, char * argv[]); \
+    bool Use##name##Add(){ g_examples.push_back(Example(#name, Use##name)); return true; } \
+    bool Use##name##Added = Use##name##Add();
+#else
+#define USE_ADD_OPENCV_EXAMPLE(name)
+#endif
+
+USE_ADD_OPENCV_EXAMPLE(FaceDetection);
+
+void Print()
+{
+    std::cout << "The list of existed examples: " << std::endl;
+    for (size_t i = 0; i < g_examples.size(); ++i)
+        std::cout << g_examples[i].name << std::endl;
+    std::cout << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "You have to set example name! For example: " << std::endl << std::endl;
+        std::cout << " Use.exe " << g_examples[0].name << std::endl << std::endl;
+        Print();
+        return 1;
+    }
+
+    std::string name = argv[1];
+    argv[1] = argv[0];
+
+    for (size_t i = 0; i < g_examples.size(); ++i)
+    {
+        if (g_examples[i].name == name)
+        {
+            std::cout << "Start '" << name << "' example: " << std::endl;
+            return g_examples[i].func(argc - 1, argv + 1);
+        }
+    }
+
+    std::cout << "You use unknown example name '" << name << "'!" << std::endl;
+    Print();
+    return 1;
+}
---
+++ src/Use/UseFaceDetection.cpp
@@ -0,0 +1,92 @@
+/*
+*  The use examples of Simd Library (http://ermig1979.github.io/Simd).
+*
+* Copyright (c) 2011-2017 Yermalayeu Ihar.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* In order to be enable of this example for Visual Studio 2015 you have to rename file 'Ocv.prop.default' into 'Ocv.prop' and set there correct paths to OpenCV.
+*/
+#ifdef SIMD_OPENCV_ENABLE
+#undef SIMD_OPENCV_ENABLE
+#define main UseFaceDetection
+
+#include <iostream>
+#include <string>
+
+#include "opencv2/opencv.hpp"
+#define SIMD_OPENCV_ENABLE
+#include "Simd/SimdDetection.hpp"
+#include "Simd/SimdDrawing.hpp"
+
+int main(int argc, char * argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "You have to set video source! It can be 0 for camera or video file name." << std::endl;
+        return 1;
+    }
+    std::string source = argv[1];
+
+    cv::VideoCapture capture;
+    if (source == "0")
+        capture.open(0);
+    else
+        capture.open(source);
+    if (!capture.isOpened())
+    {
+        std::cout << "Can't capture '" << source << "' !" << std::endl;
+        return 1;
+    }
+
+    typedef Simd::Detection<Simd::Allocator> Detection;
+    Detection detection;
+    detection.Load("../../data/cascade/haar_face_0.xml");
+    bool inited = false;
+
+    const char * WINDOW_NAME = "FaceDetection";
+    cv::namedWindow(WINDOW_NAME, 1);
+    for (;;)
+    {
+        cv::Mat frame;
+        capture >> frame;
+
+        Detection::View image = frame;
+
+        if (!inited)
+        {
+            detection.Init(image.Size(), 1.2, image.Size() / 20);
+            inited = true;
+        }
+
+        Detection::Objects objects;
+        detection.Detect(image, objects);
+
+        for (size_t i = 0; i < objects.size(); ++i)
+            Simd::DrawRectangle(image, objects[i].rect, Simd::Pixel::Bgr24(0, 255, 255));
+
+        cv::imshow(WINDOW_NAME, frame);
+        if (cvWaitKey(1) == 27)// "press 'Esc' to break video";
+            break;
+    }
+    return 0;
+}
+
+#endif //SIMD_OPENCV_ENABLE
+
