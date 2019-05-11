c4ee85ae1c2dfedca94fba2637c7cc44db3a8cfc
Yermalayeu Ihar <ermig@tut.by>
2019-05-11 14:04:01 +0300

    *fix bug: Compiler error in file TestResize.cpp (aarch64 toolchain).

--- src/Test/TestResize.cpp
+++ src/Test/TestResize.cpp
@@ -286,22 +286,21 @@ namespace Test
     {
         bool result = true;
 
+#ifndef __aarch64__         
         for (SimdResizeMethodType method = SimdResizeMethodBilinear; method <= SimdResizeMethodBilinear; method = SimdResizeMethodType(method + 1))
         {
             result = result && ResizerAutoTest(method, SimdResizeChannelByte, 1, f1, f2);
             result = result && ResizerAutoTest(method, SimdResizeChannelByte, 2, f1, f2);
             result = result && ResizerAutoTest(method, SimdResizeChannelByte, 3, f1, f2);
             result = result && ResizerAutoTest(method, SimdResizeChannelByte, 4, f1, f2);
-#ifndef __aarch64__
             result = result && ResizerAutoTest(method, SimdResizeChannelFloat, 1, f1, f2);
             result = result && ResizerAutoTest(method, SimdResizeChannelFloat, 3, f1, f2);
-#endif
         }
-
         result = result && ResizerAutoTest(SimdResizeMethodArea, SimdResizeChannelByte, 1, f1, f2);
         result = result && ResizerAutoTest(SimdResizeMethodArea, SimdResizeChannelByte, 2, f1, f2);
         result = result && ResizerAutoTest(SimdResizeMethodArea, SimdResizeChannelByte, 3, f1, f2);
         result = result && ResizerAutoTest(SimdResizeMethodArea, SimdResizeChannelByte, 4, f1, f2);
+#endif
 
         return result;
     }
