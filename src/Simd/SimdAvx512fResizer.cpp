c0eef961329d9e8d662d0fd26f44cdff55480b28
Yermalayeu Ihar <ermig@tut.by>
2021-11-12 15:59:11 +0300

    +add AVX-512F optimizations of class ResizerNearest.

--- src/Simd/SimdAvx512fResizer.cpp
+++ src/Simd/SimdAvx512fResizer.cpp
@@ -167,11 +167,80 @@ namespace Simd
 
         //---------------------------------------------------------------------
 
+        ResizerNearest::ResizerNearest(const ResParam& param)
+            : Avx2::ResizerNearest(param)
+        {
+        }
+
+        SIMD_INLINE void Gather4(const int32_t* src, const int32_t* idx, int32_t* dst, __mmask16 mask = -1)
+        {
+            __m512i _idx = _mm512_maskz_loadu_epi32(mask, idx);
+            __m512i val = _mm512_i32gather_epi32(_idx, src, 1);
+            _mm512_mask_storeu_epi32(dst, mask, val);
+        }
+
+        void ResizerNearest::Gather4(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride)
+        {
+            size_t body = AlignLo(_param.dstW, F);
+            __mmask16 tail = TailMask16(_param.dstW - body);
+            for (size_t dy = 0; dy < _param.dstH; dy++)
+            {
+                const int32_t* srcRow = (int32_t*)(src + _iy[dy] * srcStride);
+                size_t dx = 0;
+                for (; dx < body; dx += F)
+                    Avx512f::Gather4(srcRow, _ix.data + dx, (int32_t*)dst + dx);
+                if(tail)
+                    Avx512f::Gather4(srcRow, _ix.data + dx, (int32_t*)dst + dx, tail);
+                dst += dstStride;
+            }
+        }
+
+        SIMD_INLINE void Gather8(const int64_t* src, const int32_t* idx, int64_t* dst)
+        {
+            __m256i _idx = _mm256_loadu_si256((__m256i*)idx);
+            __m512i val = _mm512_i32gather_epi64(_idx, src, 1);
+            _mm512_storeu_si512((__m512i*)dst, val);
+        }
+
+        void ResizerNearest::Gather8(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride)
+        {
+            size_t body = AlignLo(_param.dstW, 8);
+            size_t tail = _param.dstW - 8;
+            for (size_t dy = 0; dy < _param.dstH; dy++)
+            {
+                const int64_t* srcRow = (int64_t*)(src + _iy[dy] * srcStride);
+                for (size_t dx = 0; dx < body; dx += 8)
+                    Avx512f::Gather8(srcRow, _ix.data + dx, (int64_t*)dst + dx);
+                Avx512f::Gather8(srcRow, _ix.data + tail, (int64_t*)dst + tail);
+                dst += dstStride;
+            }
+        }
+
+        void ResizerNearest::Run(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride)
+        {
+            Avx2::ResizerNearest::EstimateParams();
+            if (_pixelSize == 4)
+                Gather4(src, srcStride, dst, dstStride);
+            else if (_pixelSize == 8)
+                Gather8(src, srcStride, dst, dstStride);
+            else
+                Sse41::ResizerNearest::Run(src, srcStride, dst, dstStride);
+        }
+
+        bool ResizerNearest::Preferable(const ResParam& param)
+        {
+            return param.PixelSize() == 4 || (param.PixelSize() == 8 && param.dstW >= F);
+        }
+
+        //---------------------------------------------------------------------
+
         void * ResizerInit(size_t srcX, size_t srcY, size_t dstX, size_t dstY, size_t channels, SimdResizeChannelType type, SimdResizeMethodType method)
         {
             ResParam param(srcX, srcY, dstX, dstY, channels, type, method, sizeof(__m512));
             if (param.IsFloatBilinear())
                 return new ResizerFloatBilinear(param);
+            else if (param.IsNearest() && ResizerNearest::Preferable(param))
+                return new ResizerNearest(param);
             else
                 return Avx2::ResizerInit(srcX, srcY, dstX, dstY, channels, type, method);
         }
--- src/Simd/SimdResizer.h
+++ src/Simd/SimdResizer.h
@@ -388,6 +388,19 @@ namespace Simd
             ResizerFloatBilinear(const ResParam & param);
         };
 
+        class ResizerNearest : public Avx2::ResizerNearest
+        {
+        protected:
+            void Gather4(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride);
+            void Gather8(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride);
+        public:
+            ResizerNearest(const ResParam& param);
+
+            static bool Preferable(const ResParam& param);
+
+            virtual void Run(const uint8_t* src, size_t srcStride, uint8_t* dst, size_t dstStride);
+        };
+
         void * ResizerInit(size_t srcX, size_t srcY, size_t dstX, size_t dstY, size_t channels, SimdResizeChannelType type, SimdResizeMethodType method);
     }
 #endif //SIMD_AVX512F_ENABLE 
