92fd2fade3ebb7d41acbf8580af1752b54632d57
Yermalayeu Ihar <ermig@tut.by>
2022-02-03 17:38:57 +0300

    +add AVX-512BW optimizations of ResizerByteBicubic class (dense case).

--- src/Simd/SimdAvx512bwResizerBicubic.cpp
+++ src/Simd/SimdAvx512bwResizerBicubic.cpp
@@ -174,54 +174,55 @@ namespace Simd
 
         template<> SIMD_INLINE void PixelCubicSumX<1>(const uint8_t* src, const int32_t* ix, const int8_t* ax, int32_t* dst)
         {
-#if 1
-            __m128i src0 = _mm_setr_epi32(*(int32_t*)(src + ix[0]), *(int32_t*)(src + ix[1]), *(int32_t*)(src + ix[2]), *(int32_t*)(src + ix[3]));
-            __m128i src1 = _mm_setr_epi32(*(int32_t*)(src + ix[4]), *(int32_t*)(src + ix[5]), *(int32_t*)(src + ix[6]), *(int32_t*)(src + ix[7]));
-            __m256i _src = Avx2::Set(src0, src1);
-#else
-            __m256i _src = _mm256_i32gather_epi32((int32_t*)src, _mm256_loadu_si256((__m256i*)ix), 1);
-#endif
-            __m256i _ax = _mm256_loadu_si256((__m256i*)ax);
-            _mm256_storeu_si256((__m256i*)dst, _mm256_madd_epi16(_mm256_maddubs_epi16(_src, _ax), Avx2::K16_0001));
+            __m512i _src = _mm512_i32gather_epi32(_mm512_loadu_si512((__m512i*)ix), (int32_t*)src, 1);
+            __m512i _ax = _mm512_loadu_si512((__m512i*)ax);
+            _mm512_storeu_si512((__m512i*)dst, _mm512_madd_epi16(_mm512_maddubs_epi16(_src, _ax), K16_0001));
         }
 
         template<> SIMD_INLINE void PixelCubicSumX<2>(const uint8_t* src, const int32_t* ix, const int8_t* ax, int32_t* dst)
         {
-            static const __m256i PERMUTE = SIMD_MM256_SETR_EPI32(0, 0, 1, 1, 2, 2, 3, 3);
-            __m256i _ax = _mm256_permutevar8x32_epi32(_mm256_castsi128_si256(_mm_loadu_si128((__m128i*)ax)), PERMUTE);
-            static const __m256i SHUFFLE = SIMD_MM256_SETR_EPI8(
+            static const __m512i PERMUTE = SIMD_MM512_SETR_EPI32(0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7);
+            __m512i _ax = _mm512_permutexvar_epi32(PERMUTE, _mm512_castsi256_si512(_mm256_loadu_si256((__m256i*)ax)));
+            static const __m512i SHUFFLE = SIMD_MM512_SETR_EPI8(
+                0x0, 0x2, 0x4, 0x6, 0x1, 0x3, 0x5, 0x7, 0x8, 0xA, 0xC, 0xE, 0x9, 0xB, 0xD, 0xF,
+                0x0, 0x2, 0x4, 0x6, 0x1, 0x3, 0x5, 0x7, 0x8, 0xA, 0xC, 0xE, 0x9, 0xB, 0xD, 0xF,
                 0x0, 0x2, 0x4, 0x6, 0x1, 0x3, 0x5, 0x7, 0x8, 0xA, 0xC, 0xE, 0x9, 0xB, 0xD, 0xF,
                 0x0, 0x2, 0x4, 0x6, 0x1, 0x3, 0x5, 0x7, 0x8, 0xA, 0xC, 0xE, 0x9, 0xB, 0xD, 0xF);
-            __m256i _src = _mm256_shuffle_epi8(_mm256_i32gather_epi64((long long*)src, _mm_loadu_si128((__m128i*)ix), 1), SHUFFLE);
-            _mm256_storeu_si256((__m256i*)dst, _mm256_madd_epi16(_mm256_maddubs_epi16(_src, _ax), Avx2::K16_0001));
+            __m512i _src = _mm512_shuffle_epi8(_mm512_i32gather_epi64(_mm256_loadu_si256((__m256i*)ix), (long long*)src, 1), SHUFFLE);
+            _mm512_storeu_si512((__m512i*)dst, _mm512_madd_epi16(_mm512_maddubs_epi16(_src, _ax), K16_0001));
         }
 
         template<> SIMD_INLINE void PixelCubicSumX<3>(const uint8_t* src, const int32_t* ix, const int8_t* ax, int32_t* dst)
         {
-            static const __m256i PERM_A = SIMD_MM256_SETR_EPI32(0, 0, 0, 1, 1, 1, 0, 0);
-            __m256i _ax = _mm256_permutevar8x32_epi32(_mm256_castsi128_si256(_mm_loadl_epi64((__m128i*)ax)), PERM_A);
-            static const __m256i SHUFFLE = SIMD_MM256_SETR_EPI8(
+            static const __m512i PERM_1 = SIMD_MM512_SETR_EPI32(0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 0, 0, 0, 0);
+            __m512i _ax = _mm512_permutexvar_epi32(PERM_1, _mm512_castsi128_si512(_mm_loadu_si128((__m128i*)ax)));
+            static const __m512i SHUFFLE = SIMD_MM512_SETR_EPI8(
+                0x0, 0x3, 0x6, 0x9, 0x1, 0x4, 0x7, 0xA, 0x2, 0x5, 0x8, 0xB, -1, -1, -1, -1,
+                0x0, 0x3, 0x6, 0x9, 0x1, 0x4, 0x7, 0xA, 0x2, 0x5, 0x8, 0xB, -1, -1, -1, -1,
                 0x0, 0x3, 0x6, 0x9, 0x1, 0x4, 0x7, 0xA, 0x2, 0x5, 0x8, 0xB, -1, -1, -1, -1,
                 0x0, 0x3, 0x6, 0x9, 0x1, 0x4, 0x7, 0xA, 0x2, 0x5, 0x8, 0xB, -1, -1, -1, -1);
-            static const __m256i PERM_B = SIMD_MM256_SETR_EPI32(0, 1, 2, 4, 5, 6, 0, 0);
-            __m256i _src = _mm256_permutevar8x32_epi32(_mm256_shuffle_epi8(Avx2::Load<false>((__m128i*)(src + ix[0]), (__m128i*)(src + ix[1])), SHUFFLE), PERM_B);
-            _mm256_storeu_si256((__m256i*)dst, _mm256_madd_epi16(_mm256_maddubs_epi16(_src, _ax), Avx2::K16_0001));
+            static const __m512i PERM_2 = SIMD_MM512_SETR_EPI32(0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14, 0, 0, 0, 0);
+            __m512i _src = _mm512_permutexvar_epi32(PERM_2, _mm512_shuffle_epi8(
+                Load<false>((__m128i*)(src + ix[0]), (__m128i*)(src + ix[1]), (__m128i*)(src + ix[2]), (__m128i*)(src + ix[3])), SHUFFLE));
+            _mm512_storeu_si512((__m512i*)dst, _mm512_madd_epi16(_mm512_maddubs_epi16(_src, _ax), K16_0001));
         }
 
         template<> SIMD_INLINE void PixelCubicSumX<4>(const uint8_t* src, const int32_t* ix, const int8_t* ax, int32_t* dst)
         {
-            static const __m256i PERMUTE = SIMD_MM256_SETR_EPI32(0, 0, 0, 0, 1, 1, 1, 1);
-            __m256i _ax = _mm256_permutevar8x32_epi32(_mm256_castsi128_si256(_mm_loadl_epi64((__m128i*)ax)), PERMUTE);
-            static const __m256i SHUFFLE = SIMD_MM256_SETR_EPI8(
+            static const __m512i PERMUTE = SIMD_MM512_SETR_EPI32(0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3);
+            __m512i _ax = _mm512_permutexvar_epi32(PERMUTE, _mm512_castsi128_si512(_mm_loadu_si128((__m128i*)ax)));
+            static const __m512i SHUFFLE = SIMD_MM512_SETR_EPI8(
+                0x0, 0x4, 0x8, 0xC, 0x1, 0x5, 0x9, 0xD, 0x2, 0x6, 0xA, 0xE, 0x3, 0x7, 0xB, 0xF,
+                0x0, 0x4, 0x8, 0xC, 0x1, 0x5, 0x9, 0xD, 0x2, 0x6, 0xA, 0xE, 0x3, 0x7, 0xB, 0xF,
                 0x0, 0x4, 0x8, 0xC, 0x1, 0x5, 0x9, 0xD, 0x2, 0x6, 0xA, 0xE, 0x3, 0x7, 0xB, 0xF,
                 0x0, 0x4, 0x8, 0xC, 0x1, 0x5, 0x9, 0xD, 0x2, 0x6, 0xA, 0xE, 0x3, 0x7, 0xB, 0xF);
-            __m256i _src = _mm256_shuffle_epi8(Avx2::Load<false>((__m128i*)(src + ix[0]), (__m128i*)(src + ix[1])), SHUFFLE);
-            _mm256_storeu_si256((__m256i*)dst, _mm256_madd_epi16(_mm256_maddubs_epi16(_src, _ax), Avx2::K16_0001));
+            __m512i _src = _mm512_shuffle_epi8(Load<false>((__m128i*)(src + ix[0]), (__m128i*)(src + ix[1]), (__m128i*)(src + ix[2]), (__m128i*)(src + ix[3])), SHUFFLE);
+            _mm512_storeu_si512((__m512i*)dst, _mm512_madd_epi16(_mm512_maddubs_epi16(_src, _ax), K16_0001));
         }
 
         template<int N> SIMD_INLINE void RowCubicSumX(const uint8_t* src, size_t nose, size_t body, size_t tail, const int32_t* ix, const int8_t* ax, int32_t* dst)
         {
-            size_t step = 4 / N * 2;
+            size_t step = 4 / N * 4;
             size_t bodyS = nose + AlignLoAny(body - nose, step);
 
             size_t dx = 0;
