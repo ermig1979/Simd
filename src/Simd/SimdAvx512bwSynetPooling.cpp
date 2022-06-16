77fd09b16418c2e1f9f5607c2eb5400659edc931
Yermalayeu Ihar <ermig@tut.by>
2022-06-16 17:12:05 +0300

    +add Support of 3D pooling in AVX-512BW optimizations of function SynetPoolingMax32f.

--- src/Simd/SimdAvx512bwSynetPooling.cpp
+++ src/Simd/SimdAvx512bwSynetPooling.cpp
@@ -187,6 +187,257 @@ namespace Simd
                 assert(0);
         }
 
+        //-----------------------------------------------------------------------------------------
+
+        SIMD_INLINE __m512 MaxNhwcCr2(__m512 lo, __m512 hi)
+        {
+            __m512 _lo = _mm512_shuffle_f32x4(lo, hi, 0x88);
+            __m512 _hi = _mm512_shuffle_f32x4(lo, hi, 0xDD);
+            return _mm512_max_ps(_mm512_shuffle_ps(_lo, _hi, 0x88), _mm512_shuffle_ps(_lo, _hi, 0xDD));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr2_1(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                }
+                src += srcS;
+            }
+            _mm256_storeu_ps(dst, _mm512_castps512_ps256(MaxNhwcCr2(max0, _mm512_setzero_ps())));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr2_2(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                }
+                src += srcS;
+            }
+            _mm512_storeu_ps(dst, MaxNhwcCr2(max0, max1));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr2_4(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            __m512 max2 = min;
+            __m512 max3 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                    max2 = _mm512_max_ps(max2, _mm512_loadu_ps(src + w * srcC + 2 * F));
+                    max3 = _mm512_max_ps(max3, _mm512_loadu_ps(src + w * srcC + 3 * F));
+                }
+                src += srcS;
+            }
+            _mm512_storeu_ps(dst + 0 * F, MaxNhwcCr2(max0, max1));
+            _mm512_storeu_ps(dst + 1 * F, MaxNhwcCr2(max2, max3));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr2_8(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            __m512 max2 = min;
+            __m512 max3 = min;
+            __m512 max4 = min;
+            __m512 max5 = min;
+            __m512 max6 = min;
+            __m512 max7 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                    max2 = _mm512_max_ps(max2, _mm512_loadu_ps(src + w * srcC + 2 * F));
+                    max3 = _mm512_max_ps(max3, _mm512_loadu_ps(src + w * srcC + 3 * F));
+                    max4 = _mm512_max_ps(max4, _mm512_loadu_ps(src + w * srcC + 4 * F));
+                    max5 = _mm512_max_ps(max5, _mm512_loadu_ps(src + w * srcC + 5 * F));
+                    max6 = _mm512_max_ps(max6, _mm512_loadu_ps(src + w * srcC + 6 * F));
+                    max7 = _mm512_max_ps(max7, _mm512_loadu_ps(src + w * srcC + 7 * F));
+                }
+                src += srcS;
+            }
+            _mm512_storeu_ps(dst + 0 * F, MaxNhwcCr2(max0, max1));
+            _mm512_storeu_ps(dst + 1 * F, MaxNhwcCr2(max2, max3));
+            _mm512_storeu_ps(dst + 2 * F, MaxNhwcCr2(max4, max5));
+            _mm512_storeu_ps(dst + 3 * F, MaxNhwcCr2(max6, max7));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr4_1(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                }
+                src += srcS;
+            }
+            _mm_storeu_ps(dst, _mm512_castps512_ps128(MaxNhwcCr2(MaxNhwcCr2(max0, _mm512_setzero_ps()), _mm512_setzero_ps())));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr4_2(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                }
+                src += srcS;
+            }
+            _mm256_storeu_ps(dst, _mm512_castps512_ps256(MaxNhwcCr2(MaxNhwcCr2(max0, max1), _mm512_setzero_ps())));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr4_4(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            __m512 max2 = min;
+            __m512 max3 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                    max2 = _mm512_max_ps(max2, _mm512_loadu_ps(src + w * srcC + 2 * F));
+                    max3 = _mm512_max_ps(max3, _mm512_loadu_ps(src + w * srcC + 3 * F));
+                }
+                src += srcS;
+            }
+            _mm512_storeu_ps(dst + 0 * F, MaxNhwcCr2(MaxNhwcCr2(max0, max1), MaxNhwcCr2(max2, max3)));
+        }
+
+        SIMD_INLINE void PoolingMax32f3DNhwcCr4_8(const float* src, size_t srcS, size_t srcC, size_t kH, size_t kW, const __m512& min, float* dst)
+        {
+            __m512 max0 = min;
+            __m512 max1 = min;
+            __m512 max2 = min;
+            __m512 max3 = min;
+            __m512 max4 = min;
+            __m512 max5 = min;
+            __m512 max6 = min;
+            __m512 max7 = min;
+            for (size_t h = 0; h < kH; ++h)
+            {
+                for (size_t w = 0; w < kW; ++w)
+                {
+                    max0 = _mm512_max_ps(max0, _mm512_loadu_ps(src + w * srcC + 0 * F));
+                    max1 = _mm512_max_ps(max1, _mm512_loadu_ps(src + w * srcC + 1 * F));
+                    max2 = _mm512_max_ps(max2, _mm512_loadu_ps(src + w * srcC + 2 * F));
+                    max3 = _mm512_max_ps(max3, _mm512_loadu_ps(src + w * srcC + 3 * F));
+                    max4 = _mm512_max_ps(max4, _mm512_loadu_ps(src + w * srcC + 4 * F));
+                    max5 = _mm512_max_ps(max5, _mm512_loadu_ps(src + w * srcC + 5 * F));
+                    max6 = _mm512_max_ps(max6, _mm512_loadu_ps(src + w * srcC + 6 * F));
+                    max7 = _mm512_max_ps(max7, _mm512_loadu_ps(src + w * srcC + 7 * F));
+                }
+                src += srcS;
+            }
+            _mm512_storeu_ps(dst + 0 * F, MaxNhwcCr2(MaxNhwcCr2(max0, max1), MaxNhwcCr2(max2, max3)));
+            _mm512_storeu_ps(dst + 1 * F, MaxNhwcCr2(MaxNhwcCr2(max4, max5), MaxNhwcCr2(max6, max7)));
+        }
+
+        void SynetPoolingMax32f3D(const float* src, size_t srcC, size_t srcH, size_t srcW,
+            size_t kernelC, size_t kernelY, size_t kernelX, size_t strideC, size_t strideY, size_t strideX,
+            size_t padC, size_t padY, size_t padX, float* dst, size_t dstC, size_t dstH, size_t dstW, SimdTensorFormatType format)
+        {
+            if (format == SimdTensorFormatNhwc && srcC >= F)
+            {
+                if (kernelC == 2 && strideC == 2 && padC == 0 && srcC == dstC * 2)
+                {
+                    size_t srcS = srcW * srcC;
+                    size_t srcC16 = AlignLo(srcC, 16);
+                    size_t srcC32 = AlignLo(srcC, 32);
+                    size_t srcC64 = AlignLo(srcC, 64);
+                    size_t srcC128 = AlignLo(srcC, 128);
+                    __m512 min = _mm512_set1_ps(-FLT_MAX);
+                    for (size_t dh = 0; dh < dstH; ++dh)
+                    {
+                        size_t hBeg = dh * strideY - padY;
+                        size_t hEnd = Simd::Min(hBeg + kernelY, srcH);
+                        hBeg = Simd::Max<ptrdiff_t>(0, hBeg);
+                        for (size_t dw = 0; dw < dstW; ++dw)
+                        {
+                            size_t wBeg = dw * strideX - padX;
+                            size_t wEnd = Simd::Min(wBeg + kernelX, srcW);
+                            wBeg = Simd::Max<ptrdiff_t>(0, wBeg);
+                            const float* ps = src + hBeg * srcS + wBeg * srcC;
+                            size_t c = 0, d = 0;
+                            for (; c < srcC128; c += 128, d += 64)
+                                PoolingMax32f3DNhwcCr2_8(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC64; c += 64, d += 32)
+                                PoolingMax32f3DNhwcCr2_4(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC32; c += 32, d += 16)
+                                PoolingMax32f3DNhwcCr2_2(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC16; c += 16, d += 8)
+                                PoolingMax32f3DNhwcCr2_1(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            if (c < srcC)
+                                PoolingMax32f3DNhwcCr2_1(ps + srcC - 16, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + dstC - 8);
+                            dst += dstC;
+                        }
+                    }
+                    return;
+                }
+                if (kernelC == 4 && strideC == 4 && padC == 0 && srcC == dstC * 4)
+                {
+                    size_t srcS = srcW * srcC;
+                    size_t srcC16 = AlignLo(srcC, 16);
+                    size_t srcC32 = AlignLo(srcC, 32);
+                    size_t srcC64 = AlignLo(srcC, 64);
+                    size_t srcC128 = AlignLo(srcC, 128);
+                    __m512 min = _mm512_set1_ps(-FLT_MAX);
+                    for (size_t dh = 0; dh < dstH; ++dh)
+                    {
+                        size_t hBeg = dh * strideY - padY;
+                        size_t hEnd = Simd::Min(hBeg + kernelY, srcH);
+                        hBeg = Simd::Max<ptrdiff_t>(0, hBeg);
+                        for (size_t dw = 0; dw < dstW; ++dw)
+                        {
+                            size_t wBeg = dw * strideX - padX;
+                            size_t wEnd = Simd::Min(wBeg + kernelX, srcW);
+                            wBeg = Simd::Max<ptrdiff_t>(0, wBeg);
+                            const float* ps = src + hBeg * srcS + wBeg * srcC;
+                            size_t c = 0, d = 0;
+                            for (; c < srcC128; c += 128, d += 32)
+                                PoolingMax32f3DNhwcCr4_8(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC64; c += 64, d += 16)
+                                PoolingMax32f3DNhwcCr4_4(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC32; c += 32, d += 8)
+                                PoolingMax32f3DNhwcCr4_2(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            for (; c < srcC16; c += 16, d += 4)
+                                PoolingMax32f3DNhwcCr4_1(ps + c, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + d);
+                            if (c < srcC)
+                                PoolingMax32f3DNhwcCr4_1(ps + srcC - 16, srcS, srcC, hEnd - hBeg, wEnd - wBeg, min, dst + dstC - 4);
+                            dst += dstC;
+                        }
+                    }
+                    return;
+                }
+            }
+            Avx2::SynetPoolingMax32f(src, srcC, srcH, srcW, kernelC, kernelY, kernelX,
+                strideC, strideY, strideX, padC, padY, padX, dst, dstC, dstH, dstW, format);
+        }
+
         void SynetPoolingMax32f(const float* src, size_t srcC, size_t srcH, size_t srcW,
             size_t kernelC, size_t kernelY, size_t kernelX, size_t strideC, size_t strideY, size_t strideX,
             size_t padC, size_t padY, size_t padX, float* dst, size_t dstC, size_t dstH, size_t dstW, SimdTensorFormatType format)
@@ -195,7 +446,7 @@ namespace Simd
                 SynetPoolingMax32f2D(src, srcC, srcH, srcW, kernelY, kernelX,
                     strideY, strideX, padY, padX, dst, dstH, dstW, format);
             else
-                Avx2::SynetPoolingMax32f(src, srcC, srcH, srcW, kernelC, kernelY, kernelX,
+                SynetPoolingMax32f3D(src, srcC, srcH, srcW, kernelC, kernelY, kernelX,
                     strideC, strideY, strideX, padC, padY, padX, dst, dstC, dstH, dstW, format);
         }
 
