a858a73114fc8308d35f08b83fd55bad1fb5af1e
Yermalayeu Ihar <ermig@tut.by>
2025-03-10 16:15:06 +0300

    *fix bug: Error in classes Xml::NodeIterator, Xml::AttributeIterator.

--- src/Simd/SimdXml.hpp
+++ src/Simd/SimdXml.hpp
@@ -1738,7 +1738,7 @@ namespace Simd
             NodeIterator operator++(int)
             {
                 NodeIterator tmp = *this;
-                ++this;
+                ++(*this);
                 return tmp;
             }
 
@@ -1752,7 +1752,7 @@ namespace Simd
             NodeIterator operator--(int)
             {
                 NodeIterator tmp = *this;
-                ++this;
+                --(*this);
                 return tmp;
             }
 
@@ -1813,7 +1813,7 @@ namespace Simd
             AttributeIterator operator++(int)
             {
                 AttributeIterator tmp = *this;
-                ++this;
+                ++(*this);
                 return tmp;
             }
 
@@ -1827,7 +1827,7 @@ namespace Simd
             AttributeIterator operator--(int)
             {
                 AttributeIterator tmp = *this;
-                ++this;
+                --(*this);
                 return tmp;
             }
 
