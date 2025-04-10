276518c59008b8e926c6013f901b291d509ccf6b
Yermalayeu Ihar <ermig@tut.by>
2025-04-10 11:05:07 +0300

    +add SSE4.1 optimizations of class SynetConvolution16bNhwcSpecV1 (part 3: function Convolution16bNhwcSpecV1_2).

--- src/Simd/SimdSse41SynetConvolution16bNhwcSpecV1.cpp
+++ src/Simd/SimdSse41SynetConvolution16bNhwcSpecV1.cpp
@@ -130,6 +130,201 @@ namespace Simd
 
         //-----------------------------------------------------------------------------------------
 
+        template<int M> void Convolution16bNhwcSpecV1_2xM(const uint16_t* src0, const ConvParam& p, const AlgParam& a, const int* offset, size_t K, size_t dstC, int zero, const uint16_t* weight0, float* dst)
+        {
+            __m128 d00, d01, d10, d11, d20, d21, d30, d31, d40, d41, s0, w00, w01, w10, w11, m = _mm_castsi128_ps(Bf16::MASK);
+            size_t dD = a.macroD, dS = p.srcC;
+            const uint16_t* weight1 = weight0 + a.K * F;
+            const uint16_t* src1 = src0 + 1 * dS;
+            const uint16_t* src2 = src0 + 2 * dS;
+            const uint16_t* src3 = src0 + 3 * dS;
+            const uint16_t* src4 = src0 + 4 * dS;
+            if (dstC > F)
+            {
+                if (zero)
+                {
+                    if (M > 0) d00 = _mm_setzero_ps(), d01 = _mm_setzero_ps();
+                    if (M > 1) d10 = _mm_setzero_ps(), d11 = _mm_setzero_ps();
+                    if (M > 2) d20 = _mm_setzero_ps(), d21 = _mm_setzero_ps();
+                    if (M > 3) d30 = _mm_setzero_ps(), d31 = _mm_setzero_ps();
+                    if (M > 4) d40 = _mm_setzero_ps(), d41 = _mm_setzero_ps();
+                }
+                else
+                {
+                    if (M > 0) d00 = _mm_loadu_ps(dst + 0 * dD + 0), d01 = _mm_loadu_ps(dst + 0 * dD + F);
+                    if (M > 1) d10 = _mm_loadu_ps(dst + 1 * dD + 0), d11 = _mm_loadu_ps(dst + 1 * dD + F);
+                    if (M > 2) d20 = _mm_loadu_ps(dst + 2 * dD + 0), d21 = _mm_loadu_ps(dst + 2 * dD + F);
+                    if (M > 3) d30 = _mm_loadu_ps(dst + 3 * dD + 0), d31 = _mm_loadu_ps(dst + 3 * dD + F);
+                    if (M > 4) d40 = _mm_loadu_ps(dst + 4 * dD + 0), d41 = _mm_loadu_ps(dst + 4 * dD + F);
+                }
+                for (size_t k = 0; k < K; k += 2)
+                {
+                    size_t offs = *offset++;
+                    w01 = _mm_loadu_ps((float*)weight0);
+                    w00 = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(w01), Base::Bf16::SHIFT));
+                    w01 = _mm_and_ps(w01, m);
+                    w11 = _mm_loadu_ps((float*)weight1);
+                    w10 = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(w11), Base::Bf16::SHIFT));
+                    w11 = _mm_and_ps(w11, m);
+                    if (M > 0)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src0 + offs - 1)), m);
+                        d00 = _mm_add_ps(_mm_mul_ps(s0, w00), d00);
+                        d01 = _mm_add_ps(_mm_mul_ps(s0, w10), d01);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src0 + offs - 0)), m);
+                        d00 = _mm_add_ps(_mm_mul_ps(s0, w01), d00);
+                        d01 = _mm_add_ps(_mm_mul_ps(s0, w11), d01);
+                    }
+                    if (M > 1)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src1 + offs - 1)), m);
+                        d10 = _mm_add_ps(_mm_mul_ps(s0, w00), d10);
+                        d11 = _mm_add_ps(_mm_mul_ps(s0, w10), d11);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src1 + offs - 0)), m);
+                        d10 = _mm_add_ps(_mm_mul_ps(s0, w01), d10);
+                        d11 = _mm_add_ps(_mm_mul_ps(s0, w11), d11);
+                    }
+                    if (M > 2)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src2 + offs - 1)), m);
+                        d20 = _mm_add_ps(_mm_mul_ps(s0, w00), d20);
+                        d21 = _mm_add_ps(_mm_mul_ps(s0, w10), d21);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src2 + offs - 0)), m);
+                        d20 = _mm_add_ps(_mm_mul_ps(s0, w01), d20);
+                        d21 = _mm_add_ps(_mm_mul_ps(s0, w11), d21);
+                    }
+                    if (M > 3)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src3 + offs - 1)), m);
+                        d30 = _mm_add_ps(_mm_mul_ps(s0, w00), d30);
+                        d31 = _mm_add_ps(_mm_mul_ps(s0, w10), d31);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src3 + offs - 0)), m);
+                        d30 = _mm_add_ps(_mm_mul_ps(s0, w01), d30);
+                        d31 = _mm_add_ps(_mm_mul_ps(s0, w11), d31);
+                    }
+                    if (M > 4)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src4 + offs - 1)), m);
+                        d40 = _mm_add_ps(_mm_mul_ps(s0, w00), d40);
+                        d41 = _mm_add_ps(_mm_mul_ps(s0, w10), d41);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src4 + offs - 0)), m);
+                        d40 = _mm_add_ps(_mm_mul_ps(s0, w01), d40);
+                        d41 = _mm_add_ps(_mm_mul_ps(s0, w11), d41);
+                    }
+                    weight0 += DF;
+                    weight1 += DF;
+                }
+                if (M > 0) _mm_storeu_ps(dst + 0 * dD + 0, d00), _mm_storeu_ps(dst + 0 * dD + F, d01);
+                if (M > 1) _mm_storeu_ps(dst + 1 * dD + 0, d10), _mm_storeu_ps(dst + 1 * dD + F, d11);
+                if (M > 2) _mm_storeu_ps(dst + 2 * dD + 0, d20), _mm_storeu_ps(dst + 2 * dD + F, d21);
+                if (M > 3) _mm_storeu_ps(dst + 3 * dD + 0, d30), _mm_storeu_ps(dst + 3 * dD + F, d31);
+                if (M > 4) _mm_storeu_ps(dst + 4 * dD + 0, d40), _mm_storeu_ps(dst + 4 * dD + F, d41);
+            }
+            else
+            {
+                if (zero)
+                {
+                    if (M > 0) d00 = _mm_setzero_ps();
+                    if (M > 1) d10 = _mm_setzero_ps();
+                    if (M > 2) d20 = _mm_setzero_ps();
+                    if (M > 3) d30 = _mm_setzero_ps();
+                    if (M > 4) d40 = _mm_setzero_ps();
+                }
+                else
+                {
+                    if (M > 0) d00 = _mm_loadu_ps(dst + 0 * dD + 0);
+                    if (M > 1) d10 = _mm_loadu_ps(dst + 1 * dD + 0);
+                    if (M > 2) d20 = _mm_loadu_ps(dst + 2 * dD + 0);
+                    if (M > 3) d30 = _mm_loadu_ps(dst + 3 * dD + 0);
+                    if (M > 4) d40 = _mm_loadu_ps(dst + 4 * dD + 0);
+                }
+                for (size_t k = 0; k < K; k += 2)
+                {
+                    size_t offs = *offset++;
+                    w01 = _mm_loadu_ps((float*)weight0);
+                    w00 = _mm_castsi128_ps(_mm_slli_epi32(_mm_castps_si128(w01), Base::Bf16::SHIFT));
+                    w01 = _mm_and_ps(w01, m);
+                    if (M > 0)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src0 + offs - 1)), m);
+                        d00 = _mm_add_ps(_mm_mul_ps(s0, w00), d00);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src0 + offs - 0)), m);
+                        d00 = _mm_add_ps(_mm_mul_ps(s0, w01), d00);
+                    }
+                    if (M > 1)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src1 + offs - 1)), m);
+                        d10 = _mm_add_ps(_mm_mul_ps(s0, w00), d10);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src1 + offs - 0)), m);
+                        d10 = _mm_add_ps(_mm_mul_ps(s0, w01), d10);
+                    }
+                    if (M > 2)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src2 + offs - 1)), m);
+                        d20 = _mm_add_ps(_mm_mul_ps(s0, w00), d20);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src2 + offs - 0)), m);
+                        d20 = _mm_add_ps(_mm_mul_ps(s0, w01), d20);
+                    }
+                    if (M > 3)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src3 + offs - 1)), m);
+                        d30 = _mm_add_ps(_mm_mul_ps(s0, w00), d30);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src3 + offs - 0)), m);
+                        d30 = _mm_add_ps(_mm_mul_ps(s0, w01), d30);
+                    }
+                    if (M > 4)
+                    {
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src4 + offs - 1)), m);
+                        d40 = _mm_add_ps(_mm_mul_ps(s0, w00), d40);
+                        s0 = _mm_and_ps(_mm_set1_ps(*(float*)(src4 + offs - 0)), m);
+                        d40 = _mm_add_ps(_mm_mul_ps(s0, w01), d40);
+                    }
+                    weight0 += DF;
+                }
+                if (M > 0) _mm_storeu_ps(dst + 0 * dD + 0, d00);
+                if (M > 1) _mm_storeu_ps(dst + 1 * dD + 0, d10);
+                if (M > 2) _mm_storeu_ps(dst + 2 * dD + 0, d20);
+                if (M > 3) _mm_storeu_ps(dst + 3 * dD + 0, d30);
+                if (M > 4) _mm_storeu_ps(dst + 4 * dD + 0, d40);
+            }
+        }
+
+        typedef void(*Convolution16bNhwcSpecV1_2xM_Ptr)(const uint16_t* src0, const ConvParam& p, const AlgParam& a, const int* offs, size_t K, size_t dstC, int zero, const uint16_t* weight0, float* dst);
+
+        static Convolution16bNhwcSpecV1_2xM_Ptr GetConvolution16bNhwcSpecV1_2xM(size_t M)
+        {
+            switch (M)
+            {
+            case 0: return NULL;
+            case 1: return Convolution16bNhwcSpecV1_2xM<1>;
+            case 2: return Convolution16bNhwcSpecV1_2xM<2>;
+            case 3: return Convolution16bNhwcSpecV1_2xM<3>;
+            case 4: return Convolution16bNhwcSpecV1_2xM<4>;
+            case 5: return Convolution16bNhwcSpecV1_2xM<5>;
+            }
+            assert(0);
+            return NULL;
+        }
+
+        static void Convolution16bNhwcSpecV1_2(const uint16_t* src, const ConvParam& p, const AlgParam& a, const int* offs, size_t dstC, size_t dstH, size_t K, int zero, const uint16_t* weight, float* dst)
+        {
+            size_t n1 = dstH * a.srcW + 1 - p.kernelX, n = 5;
+            size_t nn = AlignLoAny(n1, n), m = n1 - nn, dW = a.K * DF;
+            size_t dD = a.macroD, dS = p.srcC;
+            Convolution16bNhwcSpecV1_2xM_Ptr convolution_2xN = GetConvolution16bNhwcSpecV1_2xM(n);
+            Convolution16bNhwcSpecV1_2xM_Ptr convolution_2xM = GetConvolution16bNhwcSpecV1_2xM(m);
+            for (size_t dc = 0; dc < dstC; dc += DF)
+            {
+                size_t dC = Simd::Min(DF, dstC - dc);
+                size_t i = 0;
+                for (; i < nn; i += n)
+                    convolution_2xN(src + i * dS, p, a, offs, K, dC, zero, weight, dst + i * dD);
+                for (; i < n1; i += m)
+                    convolution_2xM(src + i * dS, p, a, offs, K, dC, zero, weight, dst + i * dD);
+                weight += dW;
+                dst += DF;
+            }
+        }
 
         //-----------------------------------------------------------------------------------------
 
@@ -138,12 +333,12 @@ namespace Simd
         SynetConvolution16bNhwcSpecV1::SynetConvolution16bNhwcSpecV1(const ConvParam & p)
             : Base::SynetConvolution16bNhwcSpecV1(p)
         {
-            SetAlgParam(F, F * 2, 5, F * 2, Base::AlgCacheL1(), Base::AlgCacheL2(), Base::AlgCacheL3());
+            SetAlgParam(F, F * 2, 5, 2, Base::AlgCacheL1(), Base::AlgCacheL2(), Base::AlgCacheL3());
             if (_src16b)
                 _preprocess = Reorder16bNhwcSpecV1;
             else
                 _preprocess = Convert16bNhwcSpecV1;
-            //_convolution = Convolution16bNhwcSpecV0_2;
+            _convolution = Convolution16bNhwcSpecV1_2;
             //switch (p.activation)
             //{
             //case SimdConvolutionActivationIdentity: SetPostprocess<SimdConvolutionActivationRestrictRange>(p, _alg, _postprocess); break;
