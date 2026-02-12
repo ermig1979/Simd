faf70a367e3801aad136a998f4de02ade59767d8
Yermalayeu Ihar <ermig@tut.by>
2026-02-12 17:53:16 +0300

    *fix performance bugs.

--- src/Simd/SimdBaseSynetConvolution16bNhwcGemmV1.cpp
+++ src/Simd/SimdBaseSynetConvolution16bNhwcGemmV1.cpp
@@ -288,7 +288,7 @@ namespace Simd
         bool SynetConvolution16bNhwcGemmV1::CanDir2x2(const ConvParam& p)
         {
             const size_t K = p.srcC * p.kernelX * p.kernelY, M = p.dstH * p.dstW, N = p.dstC;
-            return 1 && K >= 128 && K <= 1024 && M >= 32 && M * 2 > N;
+            return 1 && K >= 128 && K <= 1024 && M >= 32 && (M * 2 > N || M < 48);
         }
 
         bool SynetConvolution16bNhwcGemmV1::CanInv4x1(const ConvParam& p)
@@ -300,7 +300,7 @@ namespace Simd
         bool SynetConvolution16bNhwcGemmV1::CanInv2x2(const ConvParam& p)
         {
             const size_t K = p.srcC * p.kernelX * p.kernelY, M = p.dstH * p.dstW, N = p.dstC;
-            return 1 && K >= 128 && K <= 1024 && N >= 32 && M >= 16;
+            return 1 && K >= 128 && K <= 1024 && N >= 32 && M >= 48;
         }
 
         bool SynetConvolution16bNhwcGemmV1::CanInv2x2_old(const ConvParam& p)
