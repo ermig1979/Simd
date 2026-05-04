edb89d9cd31b5740470cf251f6ed2c786e6073f4
Yermalayeu Ihar <ermig@tut.by>
2026-05-04 19:56:44 +0300

    *refactoring of SVE optimizations of functions AbsDifferenceSum and AbsDifferenceSumMasked.

--- src/Simd/SimdSve1AbsDifferenceSum.cpp
+++ src/Simd/SimdSve1AbsDifferenceSum.cpp
@@ -31,6 +31,14 @@ namespace Simd
 #ifdef SIMD_SVE_ENABLE
     namespace Sve
     {
+        SIMD_INLINE void AbsDifferenceSum(const uint8_t* a, const uint8_t* b, const svuint8_t& _1, const svbool_t & mask, svuint32_t & sum)
+        {
+            svuint8_t _a = svld1_u8(mask, a);
+            svuint8_t _b = svld1_u8(mask, b);
+            svuint8_t abd = svabd_x(mask, _a, _b);
+            sum = svdot_u32(sum, abd, _1);
+        }
+
         void AbsDifferenceSum(const uint8_t* a, size_t aStride, const uint8_t* b, size_t bStride, size_t width, size_t height, uint64_t* sum)
         {
             size_t A = svlen(svuint8_t());
@@ -44,19 +52,9 @@ namespace Simd
                 size_t col = 0;
                 svuint32_t _sum = svdup_n_u32(0);
                 for (; col < widthA; col += A)
-                {
-                    svuint8_t _a = svld1_u8(body, a + col);
-                    svuint8_t _b = svld1_u8(body, b + col);
-                    svuint8_t abd = svabd_x(body, _a, _b);
-                    _sum = svdot_u32(_sum, abd, _1);
-                }
+                    AbsDifferenceSum(a + col, b + col, _1, body, _sum);
                 if (widthA < width)
-                {
-                    svuint8_t _a = svld1_u8(tail, a + col);
-                    svuint8_t _b = svld1_u8(tail, b + col);
-                    svuint8_t abd = svabd_x(tail, _a, _b);
-                    _sum = svdot_u32(_sum, abd, _1);
-                }
+                    AbsDifferenceSum(a + col, b + col, _1, tail, _sum);
                 *sum += svaddv_u32(svptrue_b32(), _sum);
                 a += aStride;
                 b += bStride;
@@ -65,6 +63,16 @@ namespace Simd
 
         //--------------------------------------------------------------------------------------------------
 
+        SIMD_INLINE void AbsDifferenceSumMasked(const uint8_t* a, const uint8_t* b, const uint8_t* m, const svuint8_t& _1, const svuint8_t& index, const svbool_t& mask, svuint32_t& sum)
+        {
+            svuint8_t _a = svld1_u8(mask, a);
+            svuint8_t _b = svld1_u8(mask, b);
+            svuint8_t _m = svld1_u8(mask, m);
+            svbool_t _mask = svcmpeq_u8(mask, _m, index);
+            svuint8_t abd = svabd_z(_mask, _a, _b);
+            sum = svdot_u32(sum, abd, _1);
+        }
+
         void AbsDifferenceSumMasked(const uint8_t* a, size_t aStride, const uint8_t* b, size_t bStride,
             const uint8_t* mask, size_t maskStride, uint8_t index, size_t width, size_t height, uint64_t* sum)
         {
@@ -72,30 +80,16 @@ namespace Simd
             size_t widthA = AlignLo(width, A);
             const svbool_t body = svwhilelt_b8(size_t(0), A);
             const svbool_t tail = svwhilelt_b8(widthA, width);
-            svuint8_t _i = svdup_n_u8(index), _1 = svdup_n_u8(1);
+            svuint8_t _index = svdup_n_u8(index), _1 = svdup_n_u8(1);
             *sum = 0;
             for (size_t row = 0; row < height; ++row)
             {
                 size_t col = 0;
                 svuint32_t _sum = svdup_n_u32(0);
                 for (; col < widthA; col += A)
-                {
-                    svuint8_t _a = svld1_u8(body, a + col);
-                    svuint8_t _b = svld1_u8(body, b + col);
-                    svuint8_t _m = svld1_u8(body, mask + col);
-                    svbool_t _mask = svcmpeq_u8(body, _m, _i);
-                    svuint8_t abd = svabd_z(_mask, _a, _b);
-                    _sum = svdot_u32(_sum, abd, _1);
-                }
+                    AbsDifferenceSumMasked(a + col, b + col, mask + col, _1, _index, body, _sum);
                 if (widthA < width)
-                {
-                    svuint8_t _a = svld1_u8(tail, a + col);
-                    svuint8_t _b = svld1_u8(tail, b + col);
-                    svuint8_t _m = svld1_u8(tail, mask + col);
-                    svbool_t _mask = svcmpeq_u8(tail, _m, _i);
-                    svuint8_t abd = svabd_z(_mask, _a, _b);
-                    _sum = svdot_u32(_sum, abd, _1);
-                }
+                    AbsDifferenceSumMasked(a + col, b + col, mask + col, _1, _index, tail, _sum);
                 *sum += svaddv_u32(svptrue_b32(), _sum);
                 a += aStride;
                 b += bStride;
