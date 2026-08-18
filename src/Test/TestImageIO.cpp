c5739de3674944735ebffb5c1619a3de2560dd13
Yermalayeu Ihar <ermig@tut.by>
2026-08-18 12:57:49 +0300

    *change ImageSave tests.

--- src/Test/TestImageIO.cpp
+++ src/Test/TestImageIO.cpp
@@ -216,7 +216,7 @@ namespace Test
         std::vector<View::Format> formats({ View::Gray8, View::Bgr24, View::Bgra32, View::Rgb24, View::Rgba32});
         for (int format = 0; format < (int)formats.size(); format++)
         {
-            for (int file = (int)SimdImageFileBmp; file <= (int)SimdImageFileBmp; file++)
+            for (int file = (int)SimdImageFilePgmTxt; file <= (int)SimdImageFileBmp; file++)
             {
                 if (file == SimdImageFileJpeg)
                 {
