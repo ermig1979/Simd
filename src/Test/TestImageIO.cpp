ea77884bbcb070c6d239dc0c15e1b32a4d0a6a2c
Yermalayeu Ihar <ermig@tut.by>
2026-08-25 15:15:18 +0300

    +add image io tests.

--- src/Test/TestImageIO.cpp
+++ src/Test/TestImageIO.cpp
@@ -690,7 +690,7 @@ namespace Test
         std::vector<View::Format> formats = { View::Gray8, View::Bgr24, View::Bgra32, View::Rgb24, View::Rgba32 };
         for (size_t format = 0; format < formats.size(); format++)
         {
-            for (int file = (int)SimdImageFilePng; file <= (int)SimdImageFileBmp; file++)
+            for (int file = (int)SimdImageFilePgmTxt; file <= (int)SimdImageFileBmp; file++)
             {
                 if (file == SimdImageFileJpeg)
                 {
